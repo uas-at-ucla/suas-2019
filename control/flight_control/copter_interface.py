5d8afe580b2bb3d41e3c9f68296ddd213a68a07c
Comran Morshed <comranmorsh@gmail.com>
2017-10-06 11:05:13 -0700

    Add controller and simple takeoff test.

--- control/flight_control/copter_interface.py
+++ control/flight_control/copter_interface.py
@@ -43,6 +43,7 @@ class Sensors:
         with self.__telemetry_lock:
             self.__telemetry = {
                     "state": Sensor(str),
+                    "pixhawk_state": Sensor(str),
                     "armed": Sensor(str),
                     "voltage": Sensor(float),
                     "last_heartbeat": Sensor(float),
@@ -62,11 +63,12 @@ class Sensors:
                     "air_speed": Sensor(float)
             }
 
-    def set(self, vehicle):
+    def set(self, vehicle, controller):
         # Thread-safe method to set telemetry data by providing a Dronekit
         # vehicle instance.
         with self.__telemetry_lock:
-            self.__telemetry["state"].set(vehicle.system_status.state)
+            self.__telemetry["state"].set(controller.get_state())
+            self.__telemetry["pixhawk_state"].set(vehicle.system_status.state)
             self.__telemetry["armed"].set(vehicle.armed)
             self.__telemetry["voltage"].set(self.__cut_from_string(str( \
                     vehicle.battery), "Battery:voltage="))
@@ -119,8 +121,10 @@ class Sensors:
         return return_str
 
 class SensorReader:
-    def __init__(self, vehicle):
+    def __init__(self, vehicle, controller):
         self.vehicle = vehicle
+        self.controller = controller
+
         self.sensors = Sensors()
         self.should_run = True
 
@@ -129,24 +133,223 @@ class SensorReader:
     def __del__(self):
         self.stop()
 
+    def read_telemetry(self):
+        while self.should_run:
+            self.sensors.set(self.vehicle, self.controller)
+            time.sleep(0.1)
+
     def stop(self):
         self.should_run = False
 
-    def read_telemetry(self):
-        while self.should_run: 
-            self.sensors.set(self.vehicle)
+class Controller:
+    def __init__(self, vehicle):
+        self.vehicle = vehicle
+
+        self.print_debug("NEW LOG")
+        self.vel_lock = threading.Lock()
+        self.vel_x = 0.0
+        self.vel_y = 0.0
+        self.vel_z = 0.0
+        self.last_velocity = None
+        self.state_lock = threading.Lock()
+        self.state = "STANDBY"
+        self.should_run = True
+
+        self.reading_thread = thread.start_new_thread(self.control_loop, ())
+
+    def stop(self):
+        self.should_run = False
+
+    def print_debug(self, message):
+	msg = "(controller @ " + str(datetime.now()) + ") " + message
+        print msg
+
+    def set_state(self, state):
+        with self.state_lock:
+            state = state.replace('"', '')
+            if self.state == "FAILSAFE":
+                self.print_debug("Failed to set state: In failsafe mode.")
+                return
+
+            self.state = state
+
+    def get_state(self):
+        with self.state_lock:
+            return self.state
+
+    def phased_loop(self, frequency, last_loop):
+        # Maintain a constant phased loop by adjusting sleep based on how much
+        # time it took to run the loop function.
+        time.sleep(1.0 / frequency - min(1.0 / frequency, \
+                                         time.time() - last_loop))
+        return time.time()
+
+    def check_velocity_control(self):
+        # Make sure we are not sending stale commands.
+        with self.vel_lock:
+            if self.last_velocity is not None:
+                diff = time.time() - self.last_velocity
+                self.print_debug("Age of last velocity control from server: "
+                        + str(diff))
+
+                if diff > 0.25:
+                    # Warn when velocity control starts to lag.
+                    self.print_debug("Velocity control is lagging... " \
+                            + "diff: " + str(diff))
+                if diff > 0.50:
+                    self.print_debug("VELOCITY CONTROL IS STALE, FS ENABLED")
+                    return True
+            return False
+
+    def set_velocity(self, velocity_x, velocity_y, velocity_z):
+	# Move vehicle in direction based on specified velocity vectors.
+        # velocity_x is latitude
+        # velocity_y is longitude
+        # velocity_z is altitude (positive is down)
+
+        with self.vel_lock:
+            self.last_velocity = time.time()
+
+            self.print_debug("Setting vel x: " + str(velocity_x) \
+                    + " y: " + str(velocity_y) \
+                    + " z: " + str(velocity_z))
+
+            self.vel_x = velocity_x
+            self.vel_y = velocity_y
+            self.vel_z = velocity_z
+
+    def send_velocity(self):
+        with self.vel_lock:
+            velocity_x = self.vel_x
+            velocity_y = self.vel_y
+            velocity_z = self.vel_z
+
+        cap = 1000
+        velocity_x = max(min(cap, velocity_x), -1 * cap)
+        velocity_y = max(min(cap, velocity_y), -1 * cap)
+        cap = cap * 0.4
+        velocity_z = max(min(cap, velocity_z), -1 * cap)
+
+        msg = self.vehicle.message_factory \
+                .set_position_target_local_ned_encode(
+            0, # time_boot_ms (not used)
+            0, 0, # target system, target component
+            mavutil.mavlink.MAV_FRAME_LOCAL_NED, # frame
+            0b0000111111000111, # type_mask
+            0, 0, 0, # x, y, z positions (not used)
+            velocity_x, velocity_y, velocity_z, # 3D velocity
+            0, 0, 0, # x, y, z acceleration (not supported yet)
+            0, 0)    # yaw, yaw_rate (not supported yet)
+        self.vehicle.send_mavlink(msg)
+
+    def control_loop(self):
+        TARGET_TAKEOFF_ALT = 3.0
+
+        last_loop = 0
+
+        # Regular loop.
+        while self.should_run:
+            last_loop = self.phased_loop(10, last_loop)
+
+            new_state = None
+            current_state = self.get_state()
+
+            # Log any change in state.
+#           self.print_debug("Drone state: " + current_state)
+
+            if current_state != "STANDBY" and self.check_velocity_control():
+                self.set_state("FAILSAFE")
+                current_state = "FAILSAFE"
+
+            # Drone state machine.
+            if current_state == "STANDBY":
+                pass
+            elif current_state == "ARM":
+                self.set_state("ARM WAIT FOR INIT")
+            elif current_state == "ARM WAIT FOR INIT":
+                if self.vehicle.is_armable:
+                    self.set_state("ARMING")
+            elif current_state == "ARMING":
+                self.vehicle.mode = dronekit.VehicleMode("GUIDED")
+                self.vehicle.armed = True
+                self.set_state("ARMED CHECK")
+            elif current_state == "ARMED CHECK":
+                if self.vehicle.armed:
+                    self.set_state("ARMED")
+            elif current_state == "ARMED":
+                # Wait for other drones to also be armed.
+                self.vehicle.mode = dronekit.VehicleMode("GUIDED")
+                self.vehicle.armed = True
+                pass
+            elif current_state == "TAKEOFF":
+                self.vehicle.mode = dronekit.VehicleMode("GUIDED")
+                self.armed = True
+                self.set_state("TAKEOFF SEND COMMAND")
+            elif current_state == "TAKEOFF SEND COMMAND":
+                self.vehicle.mode = dronekit.VehicleMode("GUIDED")
+                self.armed = True
+                self.vehicle.simple_takeoff(TARGET_TAKEOFF_ALT)
+                self.set_state("TAKEOFF WAIT FOR TARGET ALTITUDE")
+            elif current_state == "TAKEOFF WAIT FOR TARGET ALTITUDE":
+                self.vehicle.mode = dronekit.VehicleMode("GUIDED")
+                self.armed = True
+                if self.vehicle.location.global_relative_frame.alt \
+                        >= TARGET_TAKEOFF_ALT * 0.85:
+                    self.set_state("TAKEN OFF")
+                else:
+                    pass
+                    #self.print_debug("Altitude: " +
+                    #    str(self.vehicle.location.global_relative_frame.alt))
+            elif current_state == "TAKEN OFF":
+                # Wait for other drones to take off.
+                self.vehicle.mode = dronekit.VehicleMode("GUIDED")
+                self.armed = True
+                pass
+            elif current_state == "VELOCITY_CONTROL":
+                self.vehicle.mode = dronekit.VehicleMode("GUIDED")
+                self.vehicle.armed = True
+                self.send_velocity()
+            elif current_state == "LAND":
+                # Command vehicle to land.
+                self.vehicle.mode = dronekit.VehicleMode("LAND")
+                self.set_state("LANDING")
+            elif current_state == "LANDING":
+                # Wait until vehicle disarms after landing.
+                if self.vehicle.armed:
+                    self.print_debug("Altitude: " +
+                        str(self.vehicle.location.global_relative_frame.alt))
+                else:
+                    self.set_state("LANDED")
+            elif current_state == "LANDED":
+                # Wait for all other drones to land.
+                pass
+            elif current_state == "FAILSAFE":
+                # Exit control loop and go into failsafe mode.
+                break
+            else:
+                self.print_debug("UNKNOWN CONTROLLER STATE: " + current_state)
+                self.set_state("FAILSAFE")
+
+        # Failsafe mode (cannot escape without restarting drone_interface).
+        self.print_debug("FAILSAFE")
+        while True:
+            # If we go into failsafe mode, continuously tell the flight
+            # controller to land.
+            self.vehicle.mode = dronekit.VehicleMode("LAND")
             time.sleep(0.1)
 
 class CopterInterface:
     def __init__(self, address):
         self.vehicle = self.__connect_to_drone(address)
-        self.sensor_reader = SensorReader(self.vehicle)
+        self.controller = Controller(self.vehicle)
+        self.sensor_reader = SensorReader(self.vehicle, self.controller)
 
     def __del__(self):
         self.stop()
 
     def stop(self):
         self.sensor_reader.stop()
+        self.controller.stop()
         self.vehicle.close()
 
     def __connect_to_drone(self, address):
--- control/test_control.py
+++ control/test_control.py
@@ -26,17 +26,18 @@ class TestControl(unittest.TestCase):
         self.test_drone.killall()
 
     def test_init(self):
+        return
         drone_address = self.spawn_simulated_drone(0.0, 0.0, 0.0, 0)
         copter = copter_interface.CopterInterface(drone_address)
 
-        time.sleep(13.0)
+        time.sleep(12.0)
 
         sensors = copter.sensor_reader.sensors.get()
-        copter.stop()
 
         # Make sure we are filling in all our sensor values after initialization
         # of the flight controller.
         self.assertTrue(sensors["state"].get() is not None)
+        self.assertTrue(sensors["pixhawk_state"].get() is not None)
         self.assertTrue(sensors["armed"].get() is not None)
         self.assertTrue(sensors["voltage"].get() is not None)
         self.assertTrue(sensors["last_heartbeat"].get() is not None)
@@ -50,27 +51,65 @@ class TestControl(unittest.TestCase):
         self.assertTrue(sensors["ground_speed"].get() is not None)
         self.assertTrue(sensors["air_speed"].get() is not None)
 
-    def test_sensors(self):
+        copter.stop()
+
+    def test_takeoff_and_land(self):
         drone_address = self.spawn_simulated_drone(0.0, 0.0, 0.0, 0)
         copter = copter_interface.CopterInterface(drone_address)
 
-        time.sleep(20.0)
+        time.sleep(4.0)
+
+        sensors = copter.sensor_reader.sensors.get()
+        print sensors["state"].get() 
+        self.assertTrue(sensors["state"].get() == "STANDBY")
+        self.assertTrue(sensors["armed"].get() == "False")
+
+        copter.controller.set_state("ARM")
+
+        for i in range(0, 18):
+            sensors = copter.sensor_reader.sensors.get()
+            print "Last heartbeat: " + str(sensors["last_heartbeat"].get()) + \
+                  " State: " + copter.controller.get_state()
+            time.sleep(1.0)
 
         # Make sure we get stable values after flight controller initialization.
+        sensors = copter.sensor_reader.sensors.get()
+
+        self.assertTrue(sensors["state"].get() == "ARMED")
+        self.assertTrue(sensors["armed"].get() == "True")
+        self.assertGreater(sensors["voltage"].get(), 12.3)
+        self.assertLess(abs(sensors["gps_rel_alt"].get()), 0.1)
+        self.assertGreater(sensors["gps_satellites"].get(), 8)
+
+        copter.controller.set_state("TAKEOFF")
+
+        print "Taking off!"
+
         for i in range(0, 10):
             sensors = copter.sensor_reader.sensors.get()
 
-            self.assertTrue(sensors["state"].get() == "STANDBY")
-            self.assertTrue(sensors["armed"].get() == "False")
-            self.assertGreater(sensors["voltage"].get(), 12.3)
-            self.assertLess(abs(sensors["velocity_x"].get()), 0.3)
-            self.assertLess(abs(sensors["velocity_y"].get()), 0.3)
-            self.assertLess(abs(sensors["velocity_z"].get()), 0.3)
-            self.assertLess(abs(sensors["gps_rel_alt"].get()), 0.1)
-            self.assertGreater(sensors["gps_satellites"].get(), 8)
-            self.assertLess(abs(sensors["ground_speed"].get()), 0.1)
-            self.assertLess(abs(sensors["air_speed"].get()), 0.1)
-            time.sleep(1)
+            print "Lat: " + '%9s' % str(sensors["gps_lat"].get()) + \
+                  " Lng: " + '%9s' % str(sensors["gps_lng"].get()) + \
+                  " Alt: " + '%9s' % str(sensors["gps_alt"].get()) + \
+                  " Ground speed: " + '%9s' % str(sensors["ground_speed"].get())
+
+            time.sleep(1.0)
+
+        self.assertLess(abs(3.0 - abs(sensors["gps_rel_alt"].get())), 0.1)
+
+        copter.controller.set_state("VELOCITY_CONTROL")
+
+        print "Switching to velocity control!"
+
+        for i in range(0, 10):
+            sensors = copter.sensor_reader.sensors.get()
+
+            print "Lat: " + '%9s' % str(sensors["gps_lat"].get()) + \
+                  " Lng: " + '%9s' % str(sensors["gps_lng"].get()) + \
+                  " Alt: " + '%9s' % str(sensors["gps_alt"].get()) + \
+                  " Ground speed: " + '%9s' % str(sensors["ground_speed"].get())
+
+            time.sleep(1.0)
 
         copter.stop()
 
@@ -85,12 +124,6 @@ class TestControl(unittest.TestCase):
 
         port = 5760 + 10 * instance
         return "tcp:127.0.0.1:" + str(port)
-
-    def spawn_drone_interface(self, drone_address):
-        self.proc_manager.spawn_process("python " + \
-                "./flight_control/copter_interface.py " + \
-                "--address " + drone_address)
-
 
 if __name__ == "__main__":
     loader = TestLoader()
