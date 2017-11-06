50a2efb12e50ce22ef7e6ff8548288ae9c8e4a7f
ruchapatki <ruchapatki@gmail.com>
2017-11-05 16:44:31 -0800

    Added blur, sizing, color, and random locations with sizing constraints.

--- vision/image_simulator/image_simulator.py
+++ vision/image_simulator/image_simulator.py
@@ -2,10 +2,11 @@ import sys
 import random, string
 import glob, os
 from PIL import Image
+from PIL import ImageFilter
 import numpy as np
+import math
 
-#TODO: scale, blur, color, (boundary conditions)
-
+#TODO: Two folders: images, txt files: one w vertices of images
 
 class ImageSimulator:
     def __init__(self):
@@ -25,32 +26,52 @@ class ImageSimulator:
             self.fields.append(Image.open(file_name).convert("RGBA"))
 
         background = self.fields[0]
-        width,height = background.size
+        backgroundWidth,backgroundHeight = background.size
 
         for i in self.targets:
 
-            #random position, angle, color values
+            #make background copy, rotate to random angle
+            backgroundCopy = background.copy()
+            angle = random.randint(0,359)
+            target = self.targets[i].rotate(angle)
 
+            #scale target size
+            scale = (random.randint(1,15))*0.1
+            targetWidth,targetHeight = target.size
+            targetWidth = int(targetWidth*scale)
+            targetHeight = int(targetHeight*scale)
+
+            targetSize = targetWidth,targetHeight
+            target = target.resize((targetWidth,targetHeight), Image.ANTIALIAS)
+            margin = 0
+            if(targetWidth > targetHeight):
+                margin = targetWidth
+            else:
+                margin = targetHeight
+
+            #randomized location for target
+            xPos = random.randint(0,backgroundWidth-margin)
+            yPos = random.randint(0,backgroundHeight-margin)
+
+            #set random color
             randomR = random.randint(0,255)
             randomG = random.randint(0,255)
             randomB = random.randint(0,255)
-            xPos = random.randint(0,width)
-            yPos = random.randint(0,height)
-            angle = random.randint(0,359)
-
-            backgroundCopy = background.copy()
-            target = self.targets[i].rotate(angle)
 
             data = np.array(target)
-            r1,g1,b1 = 0,0,0
+            r1,g1,b1 = 255,255,255
             r2,g2,b2 = randomR,randomG,randomB
 
             red, green, blue = data[:,:,0], data[:,:,1], data[:,:,2]
-            mask = (red == r1) & (green == g1) & (blue == b1)
+            mask = (red <= r1) & (green <= g1) & (blue <= b1)
             data[:,:,:3][mask] = [r2, g2, b2]
 
+            #blur target
+            blur = random.randint(0,10)
             target = Image.fromarray(data)
+            target = target.filter(ImageFilter.GaussianBlur(blur))
 
+            #paste target on field and display
             backgroundCopy.paste(target, (xPos, yPos), target)
             backgroundCopy.show()
 
