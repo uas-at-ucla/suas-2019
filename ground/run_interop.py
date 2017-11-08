fb687dc6cc7ef49753c5098ec58231ed8a373558
Ryan N <ryguyn@gmail.com>
2017-11-08 00:53:11 -0800

    Finished making interop more portable

--- ground/run_interop.py
+++ ground/run_interop.py
@@ -7,18 +7,16 @@ import sys
 sys.dont_write_bytecode = True
 sys.path.insert(0, '../util')
 
-import time
 import signal
-import subprocess
-
 import process_manager
 
 os.chdir("interop/server")
 
 interop_processes = None
+
 def signal_received(signal, frame):
-    interop_processes.run_command("docker kill interop-server")
     interop_processes.killall()
+    interop_processes.run_command("docker kill interop-server")
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_received)
 
@@ -26,15 +24,13 @@ signal.signal(signal.SIGINT, signal_received)
 # running. If it is not running (the usual case), an error is returned,
 # which can be ignored.
 interop_processes = process_manager.ProcessManager()
-interop_processes.spawn_process("docker rm -f interop-server")
-interop_processes.wait_for_complete()
+interop_processes.run_command("docker rm -f interop-server")
 
-interop_processes = process_manager.ProcessManager()
+print "SPAWN interop"
 interop_processes.spawn_process("docker run " +\
                                   "--rm " + \
                                   "--publish 8000:80 " + \
                                   "--name interop-server " + \
                                   "auvsisuas/interop-server")
-interop_processes.wait_for_complete()
 
 signal.pause()
