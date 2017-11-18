7cb745b6fbf06bc312aa43f8c93c2591f0d501f7
Ryan N <ryguyn@gmail.com>
2017-11-18 02:25:28 -0800

    Added all previous functionality to React app. Still a lot of Reactification to do.

--- ground/client/javascript/App.jsx
+++ ground/client/javascript/App.jsx
@@ -1,19 +1,15 @@
 // Home Interface
 import React, { Component } from "react";
 import Navbar from "./Navbar";
-import Panel from "./Panel";
-import Screen from "./Screen";
+import Home from "./Home";
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-
         <Navbar/>
-        <Panel/>
-        <Screen/>
-
+        <Home/>
       </div>
     );
   }
