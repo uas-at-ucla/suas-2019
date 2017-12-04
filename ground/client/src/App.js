0119bd7d7e340d6ce516641a32165cb7037e2bab
Comran Morshed <comranmorsh@gmail.com>
2017-12-03 21:48:30 -0800

    Stability modifications for ground interface.

--- ground/client/src/App.js
+++ ground/client/src/App.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client/dist/socket.io.js';
 
 import Home from './Home/Home';
 import Analytics from './Analytics/Analytics';
-import Images from './Images/Images';
+import Images from './images/Images.js';
 
 class App extends Component {
   constructor(props) {
