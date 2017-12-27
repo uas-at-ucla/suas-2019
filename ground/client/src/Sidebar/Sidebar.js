ba26e75d7c9438da38838c58f9c459336b7aa236
Comran Morshed <comranmorsh@gmail.com>
2017-12-27 03:17:54 -0800

    Switch to actual react bootstrap and improve mission planner.

--- ground/client/src/Sidebar/Sidebar.js
+++ ground/client/src/Sidebar/Sidebar.js
@@ -1,44 +1,21 @@
 import React, { Component } from 'react';
 import './Sidebar.css'
 import Panel from './Panel'
-import FlightPath from './FlightPath'
+import MissionPlanner from './MissionPlanner'
 
 class Sidebar extends Component {
   render() {
     return (
       <div className="Sidebar">
         <div id="sidebar_content">
-          <Panel title="Mission Plan" id="flightpath">
-            <FlightPath ref="flightPath"
+          <Panel id="missionPlanner">
+            <MissionPlanner ref="missionPlanner"
                         homeState={this.props.homeState}
                         setHomeState={this.props.setHomeState}/>
           </Panel>
         </div>
       </div>
     );
-  }
-
-  addCommand(lat, lng) {
-    console.log(this);
-    this.refs.flightPath.addMissionWaypoint(lat, lng);
-  }
-
-  addMissionWaypoints = () => {
-    let waypoints = this.props.homeState.waypoints.slice();
-    let mission_waypoints = this.props.homeState.mission.mission_waypoints;
-
-    for (let mission_waypoint of mission_waypoints) {
-      let waypoint = {
-        lat: mission_waypoint.latitude,
-        lng: mission_waypoint.longitude,
-        alt: mission_waypoint.altitude_msl,
-        fromMission: true
-      }
-
-      waypoints.push(waypoint);
-    }
-
-    this.props.setHomeState({waypoints: waypoints});
   }
 }
 
