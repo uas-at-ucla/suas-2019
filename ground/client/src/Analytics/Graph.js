0df8d76f0d8d6fc230e4935c88b432ce79c63d61
ivanmanan <ivanmanan@gmail.com>
2017-12-31 01:04:13 -0800

    Adding in only the Analytics files. This push does not affect anything else in the MASTER branch.

---
+++ ground/client/src/Analytics/Graph.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+// Chart libraries
+// https://github.com/jerairrest/react-chartjs-2
+// https://www.youtube.com/watch?v=Ly-9VTXJlnA
+import {Bar, Line, Pie} from 'react-chartjs-2';
+
+class Graph extends Component {
+
+  render() {
+	  const data = (canvas) => {
+		  const ctx = canvas.getContext("2d")
+		  const gradient = ctx.createLinearGradient(0,0,100,0);
+		  return {
+			   backgroundColor: gradient
+		  }
+	  }
+
+	  return (
+		  <Line data={data} />
+	  )
+  }
+}
+
+export default Graph;
