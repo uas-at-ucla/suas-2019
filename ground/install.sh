4f86d0a6863a76d454a6deba77ea51d356eb384e
ivanmanan <ivanmanan@gmail.com>
2018-01-11 22:00:03 -0800

    Attempting to install nodejs

--- ground/install.sh
+++ ground/install.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-# Update interop git submodule
-# Update node_modules
-
 # todo: should automatically install docker for us
 # Future Expansion: Get all python flask installations automatically setup here
 
+# Install Node.js
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -;
+sudo apt-get install -y nodejs;
+
+# Update git submodules
 git submodule init &&
-    git submodule update &&
+    git submodule update --recursive &&
     cd client &&
     npm install &&
-    cd ..
+    cd ..;
+
