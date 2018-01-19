92f676628f0098898a10d9310436170597c5f7d9
Comran Morshed <comranmorsh@gmail.com>
2018-01-19 11:52:04 -0800

    Styleguide cleanup.

--- ground/client/src/App.js
+++ ground/client/src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import Navbar from './Navbar/Navbar'
-import './App.css';
-import io from 'socket.io-client/dist/socket.io.js';
+import React, { Component } from "react";
+import Navbar from "./Navbar/Navbar";
+import "./App.css";
+import io from "socket.io-client/dist/socket.io.js";
 
-import Home from './Home/Home';
-import Analytics from './Analytics/Analytics';
-import Images from './Images/Images.js';
+import Home from "./Home/Home";
+import Analytics from "./Analytics/Analytics";
+import Images from "./Images/Images.js";
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      optionSelected: 'Home', // Default is Home
+      optionSelected: "Home", // Default is Home
       droneArmedStatus: "Offline",
       droneState: "",
       telemetry: null,
@@ -25,34 +26,29 @@ class App extends Component {
 
   // Render based on button clicks in Navbar
   renderSelection() {
-    switch(this.state.optionSelected) {
+    switch (this.state.optionSelected) {
       case "Analytics":
-        return (
-          <Analytics appState={this.state}/>
-        );
+        return <Analytics appState={this.state} />;
       case "Images":
-        return (
-          <Images appState={this.state}
-                  socketEmit={this.socketEmit}/>
-        );
+        return <Images appState={this.state} socketEmit={this.socketEmit} />;
       case "Home":
       default:
-        return (
-          <Home appState={this.state}
-                socketEmit={this.socketEmit}/>
-        );
+        return <Home appState={this.state} socketEmit={this.socketEmit} />;
     }
   }
 
-  setAppState = (newState) => {
+  setAppState = newState => {
     this.setState(newState);
-  }
+  };
 
   render() {
     return (
       <div className="App">
-        <Navbar appState={this.state} setAppState={this.setAppState}
-                socketEmit={this.socketEmit}/>
+        <Navbar
+          appState={this.state}
+          setAppState={this.setAppState}
+          socketEmit={this.socketEmit}
+        />
         {this.renderSelection()}
       </div>
     );
@@ -66,7 +62,7 @@ class App extends Component {
 
     this.socket = io.connect(SOCKET_ADDRESS);
 
-    this.socket.on('connect', () => {
+    this.socket.on("connect", () => {
       console.log("Connected to ground interface feeder!");
       this.setState({
         droneArmedStatus: "Online",
@@ -74,7 +70,7 @@ class App extends Component {
       });
     });
 
-    this.socket.on('disconnect', () => {
+    this.socket.on("disconnect", () => {
       console.log("Disconnected from ground interface feeder!");
       this.setState({
         droneArmedStatus: "Offline",
@@ -82,38 +78,37 @@ class App extends Component {
       });
     });
 
-    this.socket.on('drone_connected', () => {
+    this.socket.on("drone_connected", () => {
       this.setState({
         droneArmedStatus: "Starting Up Drone...",
         droneState: ""
       });
     });
 
-    this.socket.on('drone_disconnected', () => {
+    this.socket.on("drone_disconnected", () => {
       this.setState({
         droneArmedStatus: "Drone Disconnected!",
         droneState: ""
       });
     });
 
-    this.socket.on('telemetry', (telemetry) => {
+    this.socket.on("telemetry", telemetry => {
       // console.log(telemetry);
       this.setState({
         telemetry: telemetry,
         droneArmedStatus: telemetry["armed"] ? "Armed" : "Disarmed",
-        droneState: this.convert_to_title_text(telemetry["state"])
+        droneState: this.convertToTitleText(telemetry["state"])
       });
     });
 
-
-    this.socket.on('image', (data) => {
+    this.socket.on("image", data => {
       this.setState({
         testImage: data
       });
     });
 
-    this.socket.on('initial_data', (data) => {
-      this.received_interop_status(data.interop_connected);
+    this.socket.on("initial_data", data => {
+      this.receivedInteropStatus(data.interop_connected);
       if (data.interop_connected) {
         this.setState({
           stationary_obstacles: data.stationary_obstacles,
@@ -121,17 +116,17 @@ class App extends Component {
           missions: data.missions
         });
 
-        console.log("MISSION DATA!!!")
+        console.log("MISSION DATA!!!");
         console.log(data.missions[0]["fly_zones"][0]);
       }
     });
 
-    this.socket.on('moving_obstacles', (moving_obstacles) => {
-      this.setState({moving_obstacles: moving_obstacles});
+    this.socket.on("moving_obstacles", moving_obstacles => {
+      this.setState({ moving_obstacles: moving_obstacles });
     });
 
-    this.socket.on('interop_connected', (is_interop_connected) =>
-      this.received_interop_status(is_interop_connected)
+    this.socket.on("interop_connected", is_interop_connected =>
+      this.receivedInteropStatus(is_interop_connected)
     );
   }
 
@@ -141,9 +136,9 @@ class App extends Component {
     } else {
       this.socket.emit(message, data);
     }
-  }
+  };
 
-  received_interop_status(is_interop_connected) {
+  receivedInteropStatus(is_interop_connected) {
     if (is_interop_connected) {
       this.setState({
         interopBtnText: "Connected to Interop",
@@ -154,15 +149,19 @@ class App extends Component {
         interopBtnText: "Cannot Connect to Interop Server!",
         interopBtnEnabled: true
       });
-      setTimeout(() => this.setState({interopBtnText: "Connect to Interop"}), 1000);
+      setTimeout(
+        () => this.setState({ interopBtnText: "Connect to Interop" }),
+        1000
+      );
     }
   }
 
-  convert_to_title_text(str) {
-    return str.replace(/\w\S*/g, function(txt){
+  convertToTitleText(str) {
+    return str.replace(/\w\S*/g, function(txt) {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
   }
 }
 
 export default App;
+
