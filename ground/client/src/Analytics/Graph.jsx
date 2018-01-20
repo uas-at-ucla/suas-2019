9174a5fc28c4b1d95cb46e424d8945ad7f493427
Comran Morshed <comranmorsh@gmail.com>
2018-01-20 03:59:09 -0800

    Clean up battery graph.
    Former-commit-id: 00a99a5b6eccf2a2e064b875bb2f69171d2b4739

--- ground/client/src/Analytics/Graph.jsx
+++ ground/client/src/Analytics/Graph.jsx
@@ -25,6 +25,17 @@ class Graph extends Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.dataPoint) {
       this.setState((prevState, props) => {
+        let data = [
+          ...prevState.graphData.datasets[0].data,
+          nextProps.dataPoint
+        ];
+
+        // Delete old points.
+        let history = 100
+        if(data.length > history) {
+          data.splice(0, history - 10);
+        }
+
         return {
           graphData: {
             datasets: [
@@ -32,10 +43,7 @@ class Graph extends Component {
                 label: props.dataName,
                 borderColor: props.color,
                 pointBorderColor: props.color,
-                data: [
-                  ...prevState.graphData.datasets[0].data,
-                  nextProps.dataPoint
-                ]
+                data: data
               }
             ]
           }
@@ -45,13 +53,18 @@ class Graph extends Component {
   }
 
   render() {
-    if (this.props.options)
+    if (this.props.options) {
+      let height = 50;
       return (
         <Scatter
-          data={Object.assign({}, this.state.graphData, this.props.options)}
+          data={this.state.graphData}
+          options={this.props.options}
+          height={height}
         />
       );
-    else return <Scatter data={this.state.graphData} />;
+    } else {
+      return <Scatter data={this.state.graphData} />;
+    }
   }
 }
 
