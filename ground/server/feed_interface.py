f9a867a19eab75e50e622c905182c15572b754f2
Comran Morshed <comranmorsh@gmail.com>
2018-01-20 14:42:07 -0800

    Run yapf formatter on python files.

--- ground/server/feed_interface.py
+++ ground/server/feed_interface.py
@@ -28,9 +28,9 @@ stopped = False
 interop_client = None
 
 if USE_INTEROP:
-    interop_url='http://localhost:8000'
-    interop_username='testuser'
-    interop_password='testpass'
+    interop_url = 'http://localhost:8000'
+    interop_username = 'testuser'
+    interop_password = 'testpass'
     interop_client = None
     for i in range(20):
         try:
@@ -44,10 +44,12 @@ if USE_INTEROP:
             print('Waiting for Interop Server...')
             time.sleep(1)
 
+
 def signal_received(signal, frame):
     global stopped
     stopped = True
     sys.exit(0)
+
 
 @interface_socketio.on('connect')
 def connect():
@@ -69,6 +71,7 @@ def connect():
             interop_client = None
     flask_socketio.emit('initial_data', {'interop_connected': False})
 
+
 @interface_socketio.on('connect_to_interop')
 def connect_to_interop():
     if USE_INTEROP:
@@ -82,27 +85,33 @@ def connect_to_interop():
         except:
             flask_socketio.emit('interop_connected', False)
 
+
 @interface_socketio.on('moving_obstacles')
 def broadcast_moving_obstacles(moving_obstacles):
     flask_socketio.emit('moving_obstacles', moving_obstacles, \
         broadcast=True, include_self=False)
 
+
 @interface_socketio.on('execute_commands')
 def send_commands(commands):
     communications.emit('execute_commands', commands)
+
 
 @interface_socketio.on('set_state')
 def send_commands(command):
     communications.emit('set_state', command)
 
+
 @interface_socketio.on('test_image')
 def request_test_image(data):
     communications.emit('test_image', data)
+
 
 @interface_socketio.on('interop_disconnected')
 def interop_disconnected():
     flask_socketio.emit('interop_connected', False, \
         broadcast=True, include_self=False)
+
 
 def refresh_moving_obstacles():
     # This is necessary to talk to the socket from a separate thread
@@ -119,8 +128,10 @@ def refresh_moving_obstacles():
                 interop_client = None
                 interface_client.emit('interop_disconnected')
 
+
 def object_to_dict(my_object):
     return json.loads(json.dumps(my_object, default=lambda o: o.__dict__))
+
 
 def on_telemetry(*args):
     telemetry = args[0]
@@ -140,18 +151,22 @@ def on_telemetry(*args):
                 interop_client = None
                 interface_socketio.emit('interop_connected', False)
 
+
 def on_image(*args):
     interface_socketio.emit('image', args[0])
+
 
 def drone_connected():
     print "connected to drone"
     interface_socketio.emit('drone_connected')
+
 
 def drone_disconnected():
     print "disconnected from drone!"
     communications.disconnect()
     interface_socketio.emit('drone_disconnected')
     listen_for_communications()
+
 
 def listen_for_communications():
     global communications
@@ -161,6 +176,7 @@ def listen_for_communications():
     communications.on('telemetry', on_telemetry)
     communications.on('image', on_image)
     communications.wait()
+
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_received)
@@ -173,4 +189,4 @@ if __name__ == '__main__':
         t.daemon = True
         t.start()
 
-    interface_socketio.run(app, '0.0.0.0', port = 8084)
+    interface_socketio.run(app, '0.0.0.0', port=8084)
