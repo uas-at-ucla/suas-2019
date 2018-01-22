bafba84248c8d0678a11e2b861452e843a611851
Comran Morshed <comranmorsh@gmail.com>
2018-01-21 23:28:29 -0800

    Add GMaps image downloader.

--- ground/client/src/Map/Map.jsx
+++ ground/client/src/Map/Map.jsx
@@ -121,7 +121,7 @@ class Map extends Component {
     });
 
     this.map.addListener("dblclick", e => {
-      this.addGotoCommand(e.latLng.lat(), e.latLng.lng(), 80);
+      this.addGotoCommand(e.latLng.lat(), e.latLng.lng(), 30);
     });
 
     this.registerStateDepFunction(
