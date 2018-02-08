c5b19c61ab78ad35dc4d94f068ad713d808ea456
Comran Morshed <comranmorsh@gmail.com>
2018-02-07 21:57:45 -0800

    Use Google's repos for Bazel.

--- build/install.sh
+++ build/install.sh
@@ -56,10 +56,15 @@ else
 fi
 
 # Install bazel
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update && sudo apt-get -y install openjdk-8-jdk
+
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get -y install bazel
 
 ##########################################################################
 # Install all Python dependencies
-sudo apt-get install bazel
 
 ## Check if all Python packages exist; if not, then install
 if [ $OS = "Linux" ]
