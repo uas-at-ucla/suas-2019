156d2b7e9a130c97337712c719a62d969a1136f7
Comran Morshed <comranmorsh@gmail.com>
2018-02-10 21:19:44 -0800

    Add C++ sensor reader.

---
+++ src/control/io/io.h
@@ -0,0 +1,24 @@
+#ifndef SPINNY_CONTROL_IO_H_
+#define SPINNY_CONTROL_IO_H_
+
+#include "src/control/io/autopilot_interface/autopilot_interface.h"
+
+namespace spinny {
+namespace control {
+namespace io {
+
+class IO {
+ public:
+  IO();
+  void Run();
+
+ private:
+  autopilot_interface::AutopilotInterface copter_io_;
+  void RunAutopilotIO();
+};
+
+}  // namespace io
+}  // namespace control
+}  // namespace spinny
+
+#endif  // SPINNY_CONTROL_IO_H_
---
+++ src/sandbox/ipc/ipc_receiver.cc
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+int main(void) {
+  int mq; /* the queue handler */
+  /* create a key for the queue */
+  key_t k = ftok("/var", 'c');
+  if (k != -1) { /* did we succeed? */
+    /* get the queue (create if it doesn't exist) */
+    mq = msgget(k, 0644 | IPC_CREAT);
+    if (mq != -1) {     /* did we succeed */
+      char b[10000] = ""; /* a message buffer */
+      /* keep waiting on messages */
+      while (1) { /* keep going */
+        /* attempt to receive a message */
+        if (msgrcv(mq, &b, sizeof(b), 0, IPC_NOWAIT) != -1) {
+          /* was it the `stop' instruction? */
+          if (strcmp(b, "stop") == 0) break;
+          printf("%s\n", b); /* print the message */
+        } else {
+          perror("msgrcv()");
+          continue;
+          break;
+        }
+      }
+    } else {
+      perror("msgget()");
+      return 2;
+    }
+  } else {
+    perror("ftok()");
+    return 1;
+  }
+
+  return 0;
+}
