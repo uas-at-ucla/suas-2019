6c1e78fd86dcd6610e488d29e360f6f24f3ec8e2
Comran Morshed <comranmorsh@gmail.com>
2018-02-14 21:02:17 -0800

    Use python2.7.9.
    Former-commit-id: a92b19b9e2362e586c84e12cee414af38c24c903

--- tools/installation/install.sh
+++ tools/installation/install.sh
@@ -34,6 +34,16 @@ then
 fi
 
 ##########################################################################
+# Update python2.7
+sudo apt-get install -qq -y gcc-multilib g++-multilib libffi-dev libffi6 libffi6-dbg python-crypto python-mox3 python-pil python-ply libssl-dev zlib1g-dev libbz2-dev libexpat1-dev libbluetooth-dev libgdbm-dev dpkg-dev quilt autotools-dev libreadline-dev libtinfo-dev libncursesw5-dev tk-dev blt-dev libssl-dev zlib1g-dev libbz2-dev libexpat1-dev libbluetooth-dev libsqlite3-dev libgpm2 mime-support netbase net-tools bzip2
+wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
+tar xfz Python-2.7.9.tgz
+cd Python-2.7.9/
+./configure --prefix /usr/local/lib/python2.7.9 --enable-ipv6
+make
+sudo make install
+
+##########################################################################
 # Install Node.js
 
 ## See if node version is 8.x -- If not, then update node
