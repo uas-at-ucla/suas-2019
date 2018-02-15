ac3f4d9e03a567ca6a42d192602499e75acb6b70
Comran Morshed <comranmorsh@gmail.com>
2018-02-14 20:20:04 -0800

    One more fix to get sitl working with travis again.

--- tools/installation/install.sh
+++ tools/installation/install.sh
@@ -77,6 +77,7 @@ echo "";
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 sudo apt-get install gcc-6 g++-6
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6
 
 ## Check if all packages exist; if not, then install
 if [ $OS = "Linux" ]
