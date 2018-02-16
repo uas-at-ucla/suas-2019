5bff1c04fa4038ea3292d3059130dba037eb1519
Comran Morshed <comranmorsh@gmail.com>
2018-02-15 18:57:01 -0800

    Add boilerplate mission_receiver for Vivek.

---
+++ src/control/mission_receiver/mission_receiver.cc
@@ -0,0 +1,18 @@
+#include "mission_receiver.h"
+
+namespace spinny {
+namespace control {
+namespace mission_receiver {
+
+MissionReceiver::MissionReceiver() {
+
+}
+
+void MissionReceiver::Run() {
+
+}
+
+}  // namespace mission_receiver
+}  // namespace control
+}  // namespace spinny
+
