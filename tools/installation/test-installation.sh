02b84984bb4fa26ae1d7ed219a6928d4e8f4df52
Ivan Manan <ivanmanan@gmail.com>
2018-02-17 14:14:20 -0800

    Updated install script
    Former-commit-id: 0be67a877fba8493929b17b5babc45ea1eb59264

--- tools/installation/test-installation.sh
+++ tools/installation/test-installation.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "Gazebo takes forever to run if your machine has less than 4 Gb of RAM...";
-
 cd ../../;
-#bazel build //src/...;
-bazel run @PX4_sitl//:px4_sitl_visualize;
+bazel test //src/...;
+#bazel run @PX4_sitl//:px4_sitl_visualize;
