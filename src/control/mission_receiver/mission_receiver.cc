551329a131d8fe054f46be59390cc698a03f5eb1
Comran Morshed <comranmorsh@gmail.com>
2018-02-18 16:47:52 -0800

    Add zmq and protobuf message queue demo.
    Former-commit-id: 5e449e11856769d1b2716c98435def35029dadc5

--- src/control/mission_receiver/mission_receiver.cc
+++ src/control/mission_receiver/mission_receiver.cc
@@ -6,63 +6,54 @@ namespace spinny {
 namespace control {
 namespace mission_receiver {
 
-MissionReceiver *socketio_mission_receiver;
+MissionReceiver* socketio_mission_receiver;
 
-void on_connect() {
-  socketio_mission_receiver->OnConnect();
-}
+void on_connect() { socketio_mission_receiver->OnConnect(); }
 
-void on_fail() {
-  socketio_mission_receiver->OnFail();
-}
+void on_fail() { socketio_mission_receiver->OnFail(); }
 
-MissionReceiver::MissionReceiver() {
+MissionReceiver::MissionReceiver()
+    : context_(1), mission_command_stream_(context_, ZMQ_ROUTER) {
   socketio_mission_receiver = this;
 
   client_.set_open_listener(on_connect);
   client_.connect("0.0.0.0:8085");
+
+  mission_command_stream_.bind("ipc://mission_command_stream.ipc");
 }
 
-void MissionReceiver::Run() {
-
-}
+void MissionReceiver::Run() {}
 
 void MissionReceiver::OnConnect() {
   ::std::cout << "got on_connect\n";
-  client_.socket()->on("execute_commands",
-           sio::socket::event_listener_aux([&](
-               std::string const& name,
-               sio::message::ptr const& data,
-               bool isAck,
-               sio::message::list &ack_resp) {
-    (void)name;
-    (void)data;
-    (void)isAck;
-    (void)ack_resp;
+  client_.socket()->on(
+      "execute_commands",
+      sio::socket::event_listener_aux(
+          [&](std::string const& name, sio::message::ptr const& data,
+              bool isAck, sio::message::list& ack_resp) {
+            (void)name;
+            (void)data;
+            (void)isAck;
+            (void)ack_resp;
 
-    ::std::cout << "got execute_commands\n";
-  }));
+            ::std::cout << "got execute_commands\n";
+          }));
 
-  client_.socket()->on("set_state",
-           sio::socket::event_listener_aux([&](
-               std::string const& name,
-               sio::message::ptr const& data,
-               bool isAck,
-               sio::message::list &ack_resp) {
-    (void)name;
-    (void)data;
-    (void)isAck;
-    (void)ack_resp;
+  client_.socket()->on(
+      "set_state", sio::socket::event_listener_aux([&](
+                       std::string const& name, sio::message::ptr const& data,
+                       bool isAck, sio::message::list& ack_resp) {
+        (void)name;
+        (void)data;
+        (void)isAck;
+        (void)ack_resp;
 
-    ::std::cout << "got set_state\n";
-  }));
+        ::std::cout << "got set_state\n";
+      }));
 }
 
-void MissionReceiver::OnFail() {
-  ::std::cout << "socketio failed! :(\n";
-}
+void MissionReceiver::OnFail() { ::std::cout << "socketio failed! :(\n"; }
 
 }  // namespace mission_receiver
 }  // namespace control
 }  // namespace spinny
-
