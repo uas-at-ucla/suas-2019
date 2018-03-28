25a6fe20404332a55092ad19f4277cf109fc2e5b
Julien Collins <julienmcollins@gmail.com>
2018-03-28 00:06:32 -0700

    Using master version of pilot_test.cc

--- src/control/loops/pilot/pilot_test.cc
+++ src/control/loops/pilot/pilot_test.cc
@@ -10,9 +10,30 @@ namespace loops {
 namespace pilot {
 namespace testing {
 
-//constexpr double kLoopFrequency = 100;
-//constexpr double kMetersPerCoordinate = 111132.954;
-constexpr double kMetPositionTolerance = 0.5;
+const double kMetPositionTolerance = 0.5;
+const double kMetersPerCoordinate = GetDistance2D({0, 0, 0}, {1, 0, 0});
+
+class PilotPlant {
+ public:
+  PilotPlant(Position3D init_position, double loop_frequency)
+      : drone_position_(init_position), loop_frequency_(loop_frequency) {}
+
+  void MoveDrone(Vector3D flight_direction) {
+    drone_position_.latitude +=
+        flight_direction.x / loop_frequency_ / kMetersPerCoordinate;
+    drone_position_.longitude +=
+        flight_direction.y / loop_frequency_ / kMetersPerCoordinate;
+    drone_position_.altitude -= flight_direction.z / loop_frequency_;
+  }
+
+  Position3D GetPosition() { return drone_position_; }
+
+  double GetLoopFrequency() { return loop_frequency_; }
+
+ private:
+  Position3D drone_position_;
+  double loop_frequency_;
+};
 
 class PilotTest : public ::testing::Test {
  protected:
@@ -29,30 +50,25 @@ class PilotTest : public ::testing::Test {
   Pilot pilot_;
 };
 
-// TODO(comran): Uncomment this test when we make more progress on mission
-// planning.
-//TEST_F(PilotTest, ReachesGoalTest) {
-//  Position3D drone_position = {0, 0, 0};
-//  Position3D goal = {0.00001, 0.00001, 100};
+TEST_F(PilotTest, ReachesGoalTest) {
+  PilotPlant plant({0, 0, 0}, 100);
+  Position3D goal = {0.0000002, 0.000003, 10};
 
-//  for (int i = 0;
-//       i < 100 * kLoopFrequency && !CheckMetGoal(drone_position, goal); i++) {
-//    PilotOutput pilot_output = pilot_.Calculate(drone_position);
+  double runtime_in_seconds = 10;
 
-//    Vector3D flight_direction = pilot_output.flight_velocities;
+  for (int i = 0; i < runtime_in_seconds * plant.GetLoopFrequency() &&
+                  !CheckMetGoal(plant.GetPosition(), goal);
+       i++) {
+    Vector3D flight_direction = pilot_.Calculate(plant.GetPosition(), goal);
 
-//    drone_position.latitude +=
-//        flight_direction.x / kLoopFrequency / kMetersPerCoordinate;
-//    drone_position.longitude +=
-//        flight_direction.y / kLoopFrequency / kMetersPerCoordinate;
-//    drone_position.altitude -= flight_direction.z / kLoopFrequency;
-//  }
+    plant.MoveDrone(flight_direction);
+  }
 
-//  MetGoal(drone_position, goal);
-//}
+  MetGoal(plant.GetPosition(), goal);
+}
 
 }  // namespace testing
 }  // namespace pilot
 }  // namespace loops
 }  // namespace control
-}  // namespace spinny
+}  // namespace src
