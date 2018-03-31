64cbbbc7cbb2d226a2e7bbb74cc9fab68fb1bcb5
Comran Morshed <comranmorsh@gmail.com>
2018-03-30 23:19:28 -0700

    Add mission receiver to pilot.
    Former-commit-id: f3e8505a03609dc4aef57d5eb3282f8360221527

--- src/control/loops/pilot/pilot.cc
+++ src/control/loops/pilot/pilot.cc
@@ -5,66 +5,11 @@ namespace control {
 namespace loops {
 namespace pilot {
 
-PilotMissionHandler::PilotMissionHandler(::lib::MissionManager *mission_manager)
-    : mission_manager_(mission_manager) {}
-
-void PilotMissionHandler::operator()() {
-  // Listen to ZMQ message queue and update MissionManager with any new missions
-  // that come along.
-  ::zmq::context_t context(1);
-  ::zmq::socket_t ground_communicator_stream(context, ZMQ_REQ);
-  ground_communicator_stream.connect("ipc:///tmp/mission_command_stream.ipc");
-
-  while (true) {
-    ::zmq::message_t mission_message;
-    ground_communicator_stream.recv(&mission_message);
-    ::std::string mission_message_string(
-        static_cast<char *>(mission_message.data()), mission_message.size());
-
-    ::src::controls::ground_communicator::Mission mission_protobuf;
-    mission_protobuf.ParseFromString(mission_message_string);
-
-    mission_manager_->AddCommands(ParseMissionProtobuf(mission_protobuf));
-  }
-}
-
-::std::vector<::std::shared_ptr<::lib::MissionCommand>>
-PilotMissionHandler::ParseMissionProtobuf(
-    ::src::controls::ground_communicator::Mission mission_protobuf) {
-  ::std::vector<::std::shared_ptr<::lib::MissionCommand>> new_commands;
-
-  // Iterate through all commands in the protobuf and convert them to our C++
-  // objects for commands.
-  for (::src::controls::ground_communicator::Command cmd_protobuf :
-       mission_protobuf.commands()) {
-    if (cmd_protobuf.type() == "goto") {
-      new_commands.push_back(::std::make_shared<::lib::MissionCommandGoto>(
-          new ::lib::MissionCommandGoto(cmd_protobuf.latitude(),
-                                        cmd_protobuf.longitude(),
-                                        cmd_protobuf.altitude())));
-
-    } else if (cmd_protobuf.type() == "bomb") {
-      new_commands.push_back(::std::make_shared<::lib::MissionCommandBombDrop>(
-          new ::lib::MissionCommandBombDrop()));
-
-    } else {
-      // Return an empty command protobuf if we encounter any parsing errors
-      // with the mission given.
-      ::std::cerr << "ERROR: Invalid command type " << cmd_protobuf.type()
-                  << ::std::endl;
-
-      return ::std::vector<::std::shared_ptr<::lib::MissionCommand>>();
-    }
-  }
-
-  return new_commands;
-}
-
-Pilot::Pilot() : pilot_mission_handler_(&mission_manager_) {}
+Pilot::Pilot() {}
 
 PilotOutput Pilot::Calculate(Position3D drone_position) {
   ::std::shared_ptr<::lib::MissionCommand> cmd_ptr =
-      mission_manager_.GetCurrentCommand();
+      mission_message_queue_receiver_.get_mission_manager()->GetCurrentCommand();
 
   Vector3D flight_direction;
   bool bomb_drop = false;
@@ -99,11 +44,6 @@ PilotOutput Pilot::Calculate(Position3D drone_position) {
   }
 
   return {flight_direction, bomb_drop};
-}
-
-void Pilot::HandleMission() {
-  ::std::cerr << "Able to handle mission from mission_handler" << ::std::endl;
-  pilot_mission_handler_();
 }
 
 }  // namespace pilot
