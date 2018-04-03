38d10b45f1bc432bab751bde4effd7a2c1d1510e
Comran Morshed <comranmorsh@gmail.com>
2018-04-03 03:51:18 -0700

    Add alarm controller.
    Former-commit-id: e9bdc8cb70c755d5c6a7e3bb7ce33173d28d6681

---
+++ lib/alarm/alarm_test.cc
@@ -0,0 +1,41 @@
+#include "gtest/gtest.h"
+
+#include "lib/alarm/alarm.h"
+
+namespace lib {
+namespace alarm {
+namespace testing {
+
+TEST(AlarmTest, AlarmRoutineTest) {
+  double frequency = 1e2;
+  Alarm alarm(frequency);
+
+  alarm.AddAlert({0.5, 0.5});
+  alarm.AddAlert({0.25, 0.25});
+  alarm.AddAlert({0, 0.25});
+  alarm.AddAlert({0.25, 0});
+
+  double time = 0;
+  for (; time < 3; time += 1 / frequency) {
+    bool should_alarm = alarm.ShouldAlarm();
+
+    EXPECT_FALSE(!(should_alarm == true) && time >= 0 && time < 0.5);
+    EXPECT_FALSE(!(should_alarm == false) && time >= 0.5 && time < 1.0);
+    EXPECT_FALSE(!(should_alarm == true) && time >= 1.0 && time < 1.25);
+    EXPECT_FALSE(!(should_alarm == false) && time >= 1.25 && time < 1.75);
+    EXPECT_FALSE(!(should_alarm == true) && time >= 1.75 && time < 2.0);
+    EXPECT_FALSE(!(should_alarm == false) && time >= 2.0);
+  }
+
+  alarm.AddAlert({0.25, 0.25});
+  for (; time < 10; time += 1 / frequency) {
+    bool should_alarm = alarm.ShouldAlarm();
+
+    EXPECT_FALSE(!(should_alarm == true) && time >= 3.0 && time < 3.25);
+    EXPECT_FALSE(!(should_alarm == false) && time >= 3.25);
+  }
+}
+
+}  // namespace testing
+}  // namespace larm
+}  // namespace lib
