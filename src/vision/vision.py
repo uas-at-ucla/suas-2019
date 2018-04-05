c4c8f792e69d9bde5c6a430a29d6f0747dc32e74
Ben Limpanukorn <ben.limpanukorn@gmail.com>
2018-04-05 00:20:33 -0700

    Add Client sub-sub-parsers
    Former-commit-id: 2de8ce4893b249660af61940ac65b058c745d123

--- src/vision/vision.py
+++ src/vision/vision.py
@@ -22,77 +22,89 @@ task_queue = []
 #     def __init__(self, image_path):
 #         self._image_path = image_path
 #     def execute(self):
-#         # 
+#         #
+
 
 def signal_received(signal, frame):
     # Shutdown all the spawned processes and exit cleanly.
     processes.killall()
     sys.exit(0)
 
+
 # Server #######################################################################
 socketio_app = Flask(__name__)
 socketio_app.config['SECRET_KEY'] = 'flappy'
 vision_socketio_server = flask_socketio.SocketIO(socketio_app)
+
+
 @vision_socketio_server.on('connect')
 def vision_socketio_server_connect():
     print("Someone connected to vision server")
 
+
 @vision_socketio_server.on('process_image')
 def process_image(json):
     print("Telling rsync clients to download image")
-    vision_socketio_server.emit('rsync', {'file_path':json['file_path']})
+    vision_socketio_server.emit('rsync', {'file_path': json['file_path']})
+
 
 @vision_socketio_server.on('download_complete')
 def call_yolo(json):
     print('Download Complete -> Telling yolo client to run on the image')
-    vision_socketio_server.emit('yolo', {'file_path':json['local_file_path']})
+    vision_socketio_server.emit('yolo', {'file_path': json['local_file_path']})
+
 
 def server_worker(args):
     vision_socketio_server.run(socketio_app, '0.0.0.0', port=int(args.port))
 
+
 # Clients ######################################################################
 vision_client = None
 
-def connect_to_server(ip, port):
+def client_worker(args):
+    ip_and_port = args.vision_srv.split(':')
+    ip = ip_and_port[0]
+    port = int(ip_and_port[1])
+
+    print('Attempting to connect to server @ ' + ip + str(port))
     global vision_client
     vision_client = socketIO_client.SocketIO(ip, port=port)
+    print('Connected to server!')
+
 
 # Rsync file synchronization ###################################################
 def rsync_worker(args):
     # rsync -avz --progress -e "ssh -p 22" "user@server.org:~/file.cc" .
-    ip_and_port = args.vision_srv.split(':')
-    ip = ip_and_port[0]
-    port = int(ip_and_port[1])
-        
-    print('Attempting to connect to server @ ' + ip + str(port))
-    connect_to_server(ip, port)
-    print('Connected to server!')
+    client_worker(args)
     vision_client.on('rsync', run_rsync)
     vision_client.wait()
-    
+
+
 def run_rsync(*args):
     # TODO Use rsync to download the image with the given filepath
-    # processes.spawn_process('something with rsync')
+    processes.spawn_process('rsync -vz --progress -e "ssh -p 22" ""')
     print('Called rsync with args: <' + '> <'.join(map(str, args)) + '>')
-    vision_client.emit('download_complete', {'local_file_path': 'dummy_local_path'})
+    vision_client.emit('download_complete',
+                       {'local_file_path': 'dummy_local_path'})
+
 
 # YOLO image classification ####################################################
 def yolo_worker(args):
-    ip_and_port = args.vision_srv.split(':')
-    ip = ip_and_port[0]
-    port = int(ip_and_port[1])
-
-    print('Attempting to connect to server @ ' + ip + str(port))
-    connect_to_server(ip, port)
-    print('Connected to server!')
+    client_worker(args)
     vision_client.on('yolo', run_yolo)
     vision_client.wait()
+
 
 def run_yolo(*args):
     # TODO Use yolo to identify the image with the given filepath and cut out the targets
     # processes.spawn_process('something with rsync')
     print('Called yolo with args: <' + '> <'.join(map(str, args)) + '>')
-    vision_client.emit('targets_located', {'file_path': 'dummy_local_path', 'localized_targets_path': 'dummy_targets_path'})
+    vision_client.emit(
+        'targets_located', {
+            'file_path': 'dummy_local_path',
+            'localized_targets_path': 'dummy_targets_path'
+        })
+
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_received)
@@ -100,21 +112,27 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(help='sub-command help')
 
+    # Server Parser ####################################################
     server_port = None
     server_parser = subparsers.add_parser('server', help='server help')
-    server_parser.add_argument("--port", action='store', dest='port',
-            required=True)
+    server_parser.add_argument(
+        "--port", action='store', dest='port', required=True)
     server_parser.set_defaults(func=server_worker)
 
-    rsync_url = None
-    rsync_parser = subparsers.add_parser('rsync', help='rsync help')
-    # rsync_parser.add_argument("--src", action='store', dest='src',
-    #        required=True)
-    rsync_parser.add_argument("--vision_srv", action='store', dest='vision_srv', required=True)
+    # Client Parsers ###################################################
+    client_parser = subparsers.add_parser('client', help='client help')
+    client_parser.add_argument(
+        "--vision-srv", action='store', dest='vision_srv', required=True)
+    client_subparsers = client_parser.add_subparsers()
+
+    # Rsync specific args 
+    rsync_parser = client_subparsers.add_parser('rsync', help='rsync help')
+    rsync_parser.add_argument(
+        "--img-src-dir", action='store', dest='img_src_dir', required=True)
     rsync_parser.set_defaults(func=rsync_worker)
 
-    yolo_parser = subparsers.add_parser('yolo', help='yolo help')
-    rsync_parser.add_argument("--vision_srv", action='store', dest='vision_srv', required=True)
+    # Yolo specific args
+    yolo_parser = subparsers.client_subparsers('yolo', help='yolo help')
     yolo_parser.set_defaults(func=yolo_worker)
 
     args = parser.parse_args()
