7b5cb039c6aa8e19d1520dab357727e15ab0fe8c
Ben Limpanukorn <ben.limpanukorn@gmail.com>
2018-04-14 21:36:08 -0700

    Add yolo weights

--- src/vision/localizer/predict.py
+++ src/vision/localizer/predict.py
@@ -2,6 +2,7 @@ from darkflow.net.build import TFNet
 import cv2
 import random
 import signal
+import sys
 
 def randomColor():
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
@@ -13,13 +14,13 @@ signal.signal(signal.SIGINT, signal_received)
 
 options = {
     "model": "cfg/yolo-auvsi.cfg",
-    "load": "bin/tiny-yolo-voc.weights",
+    "load": "darkflow/bin/tiny-yolo-voc.weights",
     "threshold": 0.1
 }
 
 tfnet = TFNet(options)
 
-img = cv2.imread("sample_img/0000002.jpg")
+img = cv2.imread(sys.argv[1])
 
 results = tfnet.return_predict(img)
 
