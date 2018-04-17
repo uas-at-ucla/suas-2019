0c71e7e222262ae3d5ce2eb2735005d57cdddc51
Comran Morshed <comranmorsh@gmail.com>
2018-04-17 03:34:04 -0700

    Start work on mission manager preprocessor.
    Former-commit-id: 37f906664c85c59cc61f0dea2422674a4ef36a39

--- lib/mission_manager/mission_manager.h
+++ lib/mission_manager/mission_manager.h
@@ -1,10 +1,11 @@
 #ifndef SPINNY_LIB_MISSION_MANAGER_MISSION_MANAGER_H_
 #define SPINNY_LIB_MISSION_MANAGER_MISSION_MANAGER_H_
 
-#include <mutex>
+#include <algorithm>
 #include <condition_variable>
 #include <iostream>
 #include <memory>
+#include <mutex>
 #include <vector>
 
 #include "lib/mission_manager/mission_commands.pb.h"
@@ -34,9 +35,14 @@ class MissionManager {
 
   int GetCurrentCommandIndex();
   ::lib::mission_manager::Command GetCurrentCommand();
+  void UnrollMission(
+      ::lib::mission_manager::Mission *mission);
+  void Preprocess();
+  void DumpMission();
 
  private:
   ::lib::mission_manager::Mission mission_;
+  void DumpMission(::lib::mission_manager::Mission, int nest);
 
   Semaphore semaphore_;
   int command_pointer_;
