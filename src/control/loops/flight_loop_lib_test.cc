c722961f7726afff54804c1c39ce9f7aa339610f
Comran Morshed <comranmorsh@gmail.com>
2018-04-24 15:10:49 -0700

    Fix failing tests due to arming routine timing out.

--- src/control/loops/flight_loop_lib_test.cc
+++ src/control/loops/flight_loop_lib_test.cc
@@ -210,7 +210,7 @@ TEST_F(FlightLoopTest, ArmTakeoffAndLandCheck) {
   // Do takeoff and check if times out.
   ::std::cout << "sending mission...\n";
   for (int i = 0;
-       i < 3000 && (flight_loop_queue.sensors->relative_altitude < 2.2 ||
+       i < 5000 && (flight_loop_queue.sensors->relative_altitude < 2.2 ||
                     !flight_loop_queue.sensors->armed);
        i++) {
     flight_loop_queue_.goal.MakeWithBuilder().run_mission(true).Send();
