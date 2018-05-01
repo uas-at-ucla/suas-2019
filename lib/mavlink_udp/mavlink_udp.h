e193ad0b6263b43097571718e59748126d19094f
Comran Morshed <comranmorsh@gmail.com>
2018-05-01 12:12:29 -0700

    Use UDP and MAVProxy to manage communication with PX4.

--- lib/mavlink_udp/mavlink_udp.h
+++ lib/mavlink_udp/mavlink_udp.h
@@ -1,31 +1,33 @@
 #ifndef LIB_SERIAL_SERIAL_PORT_H_
 #define LIB_SERIAL_SERIAL_PORT_H_
 
+#include <unistd.h>
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
-#include <unistd.h> 
 
-#include "zmq.hpp"
 #include <common/mavlink.h>
+#include <boost/asio.hpp>
 
 namespace lib {
 namespace mavlink_udp {
 
-class MavlinkUDP {
+class MavlinkUDP : public std::enable_shared_from_this<MavlinkUDP> {
  public:
-  MavlinkUDP(int port);
+  MavlinkUDP();
   ~MavlinkUDP();
 
   int read_messages(::std::vector<mavlink_message_t> &message);
   int write_message(const mavlink_message_t &message);
 
  private:
-  ::zmq::context_t context_;
-  ::zmq::socket_t socket_;
-
   mavlink_status_t lastStatus;
   pthread_mutex_t lock;
+
+  ::boost::asio::io_service io_service_;
+  std::atomic<bool> remote_exists_;
+  ::boost::asio::ip::udp::socket socket_, remote_ep_, last_remote_ep_, bind_ep_;
 };
 
 }  // namespace mavlink_udp
