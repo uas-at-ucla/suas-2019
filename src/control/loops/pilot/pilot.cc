610e96859b56d0943511f5bbe09d65d3add747e5
Comran Morshed <comranmorsh@gmail.com>
2018-05-02 02:41:36 -0700

    Add tunneling algorithm for navigating drone with goto_raw commands.

--- src/control/loops/pilot/pilot.cc
+++ src/control/loops/pilot/pilot.cc
@@ -6,7 +6,8 @@ namespace loops {
 namespace pilot {
 
 Pilot::Pilot()
-    : drone_position_set_(false),
+    : cmd_set_(false),
+      drone_position_set_(false),
       drone_position_semaphore_(1),
       thread_(&Pilot::PreprocessorThread, this) {}
 
@@ -16,22 +17,6 @@ Pilot::~Pilot() {
 }
 
 void Pilot::PreprocessorThread() {
-  // Add test obstacle while developing.
-  // TODO(comran): Remove.
-//::lib::mission_manager::Obstacles obstacles;
-//::lib::mission_manager::StaticObstacle *obstacle =
-//    obstacles.add_static_obstacles();
-//::lib::mission_manager::Position2D *location =
-//    new ::lib::mission_manager::Position2D();
-
-//location->set_latitude(34.1747796812899);
-//location->set_longitude(-118.48062618357);
-//obstacle->set_allocated_location(location);
-
-//obstacle->set_cylinder_radius(80);
-
-//mission_message_queue_receiver_.SetObstacles(obstacles);
-
   while (run_) {
     if (drone_position_set_) {
       drone_position_semaphore_.Wait();
@@ -47,6 +32,7 @@ void Pilot::PreprocessorThread() {
 
 PilotOutput Pilot::Calculate(Position3D drone_position) {
   drone_position_semaphore_.Wait();
+
   drone_position_ = drone_position;
   drone_position_set_ = true;
   drone_position_semaphore_.Notify();
@@ -54,6 +40,20 @@ PilotOutput Pilot::Calculate(Position3D drone_position) {
   ::lib::mission_manager::Command cmd =
       mission_message_queue_receiver_.get_mission_manager()
           ->GetCurrentCommand();
+
+  // Keep track of the current and previous commands for the current mission.
+  if (cmd_set_) {
+    if (!google::protobuf::util::MessageDifferencer::Equals(cmd, cmd_)) {
+      ::std::cout << "DIFFERENCE------------------------------------------\n";
+      last_cmd_ = cmd_;
+    }
+  } else {
+    ::std::cout << "NOTHING------------------------\n";
+    last_cmd_.mutable_nothingcommand();
+    cmd_set_ = true;
+  }
+
+  cmd_ = cmd;
 
   Vector3D flight_direction = {0, 0, 0};
   bool bomb_drop = false;
@@ -64,16 +64,101 @@ PilotOutput Pilot::Calculate(Position3D drone_position) {
   } else if (cmd.has_sleepcommand()) {
     // Sleep.
   } else if (cmd.has_gotorawcommand()) {
+    // Go directly to a location on a field.
     constexpr double kSpeed = 12.0;
 
-    Position3D goal = {cmd.gotorawcommand().goal().latitude(),
-                       cmd.gotorawcommand().goal().longitude(),
-                       cmd.gotorawcommand().goal().altitude()};
+    if (!last_cmd_.has_gotorawcommand()) {
+      // Create a mock goto_raw location at the drone's current position to make
+      // the following drone routing algorithm work.
 
-    flight_direction = PointTowards(drone_position, goal);
+      ::lib::mission_manager::Position3D *goal =
+          last_cmd_.mutable_gotorawcommand()->mutable_goal();
+      goal->set_latitude(drone_position.latitude);
+      goal->set_longitude(drone_position.longitude);
+      goal->set_altitude(drone_position.altitude);
+    }
+
+    // Funneling algorithm for keeping the drone on a defined path on its way to
+    // the goal.
+
+    //       drone
+    //         \\
+    //          \\
+    //           \\______________
+    //                           \_______
+    // origin                            \--------------------------------- goal
+
+    Position3D goal = {cmd_.gotorawcommand().goal().latitude(),
+                       cmd_.gotorawcommand().goal().longitude(),
+                       cmd_.gotorawcommand().goal().altitude()};
+    Position3D last_goal = {last_cmd_.gotorawcommand().goal().latitude(),
+                            last_cmd_.gotorawcommand().goal().longitude(),
+                            last_cmd_.gotorawcommand().goal().altitude()};
+
+    //                  drone
+    //                    |\
+    //                    | \
+    //                    |  \
+    //                    |   \
+    //                    |    \
+    //                    |   distance
+    //                    |      \
+    //                    |       \
+    //                 deviation   \
+    //                    |         \
+    //                    |          \
+    //                    |           \
+    //                    |            \
+    //                    |             \
+    //                    |              \
+    // origin  ...path... |-- projected --\ goal
+    //                        distance
+    ::Eigen::Vector3d distance_vector(
+        GetDistance2D({0, 0, 0}, {0, 1, 0}) *
+            (goal.longitude - drone_position.longitude),
+        GetDistance2D({0, 0, 0}, {1, 0, 0}) *
+            (goal.latitude - drone_position.latitude),
+        drone_position.altitude - goal.altitude);
+
+    ::Eigen::Vector3d path_vector(GetDistance2D({0, 0, 0}, {0, 1, 0}) *
+                                      (goal.longitude - last_goal.longitude),
+                                  GetDistance2D({0, 0, 0}, {1, 0, 0}) *
+                                      (goal.latitude - last_goal.latitude),
+                                  goal.altitude - last_goal.altitude);
+    ::std::cout << "PATH VECTOR: " << path_vector << ::std::endl;
+
+    double distance = distance_vector.norm();
+
+    ::Eigen::Vector3d path_projection = path_vector *
+                                        path_vector.dot(distance_vector) /
+                                        path_vector.dot(path_vector);
+
+    ::Eigen::Vector3d error = distance_vector - path_projection;
+    ::std::cout << error << ::std::endl;
+
+    ::Eigen::Vector3d adjustment = error / 15;
+    double mix = ::std::min(1.0, ::std::pow(adjustment.norm(), 1.5));
+
+    ::Eigen::Vector3d flight_direction_vector =
+        (path_vector / path_vector.norm()) * (1 - mix) +
+        (adjustment / adjustment.norm()) * (mix);
+
+    double projected_distance = path_projection.norm();
+
+    ::std::cout << "distance: " << distance << ::std::endl;
+    ::std::cout << "projected: " << projected_distance << ::std::endl;
+    ::std::cout << "mix: " << mix << ::std::endl;
+    ::std::cout << "adjustment: " << adjustment.norm() << ::std::endl;
+
+    // 0 is aim at goal, 1 is move drone towards path.
+    ::std::cout << flight_direction_vector << ::std::endl;
+
+    flight_direction = {flight_direction_vector.y(),
+                        flight_direction_vector.x(),
+                        flight_direction_vector.z()};
     flight_direction *= kSpeed;
 
-    if (GetDistance2D(drone_position, goal) < kSpeed / 5) {
+    if (GetDistance2D(drone_position, goal) < kSpeed) {
       mission_message_queue_receiver_.get_mission_manager()->PopCommand();
     }
   } else if (cmd.has_bombdropcommand()) {
@@ -84,11 +169,6 @@ PilotOutput Pilot::Calculate(Position3D drone_position) {
   }
 
   return {flight_direction, bomb_drop};
-}
-
-int Pilot::GetCurrentCommandIndex() {
-  // DEPRECATED: DON'T USE.
-  return -1;
 }
 
 void Pilot::SetMission(::lib::mission_manager::Mission mission) {
