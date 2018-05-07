c9a5981e211a598289dba902bd4451d762d58b77
Comran Morshed <comranmorsh@gmail.com>
2018-05-06 20:22:54 -0700

    Add new mavproxy deploy and changes from test flight.
    Former-commit-id: e206bb83c8c310f236b0c5db97f1f86e9d42ef51

--- lib/scripts/start_drone_code.sh
+++ lib/scripts/start_drone_code.sh
@@ -5,6 +5,9 @@ killall core
 killall flight_loop
 killall io
 killall ground_communicator
+killall log_writer
+killall screen
+killall mavproxy.py
 ipcrm --all
 
 # Start code in background.
@@ -14,3 +17,4 @@ screen -d -m /home/pi/suas_2018_deploy/flight_loop &
 screen -d -m /home/pi/suas_2018_deploy/io &
 screen -d -m /home/pi/suas_2018_deploy/ground_communicator &
 screen -d -m /home/pi/suas_2018_deploy/log_writer &
+screen -d -m /home/pi/suas_2018_deploy/run_mavproxy.sh &
