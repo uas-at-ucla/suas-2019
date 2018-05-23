794680690e124263f3944122d9475703533a8943
Comran Morshed <comranmorsh@gmail.com>
2018-05-22 23:33:36 -0700

    Make dslr_interface work reliably with hardware.

--- lib/scripts/download_photos.sh
+++ lib/scripts/download_photos.sh
@@ -5,6 +5,7 @@ mkdir /tmp/suas_2018_dslr_mounted
 mkdir downloaded
 
 killall gphotofs
-gphotofs --port 8081 /tmp/suas_2018_dslr_mounted
+/usr/bin/gphotofs /tmp/suas_2018_dslr_mounted
 
 rsync -avP --progress /tmp/suas_2018_dslr_mounted downloaded
+fusermount -u /tmp/suas_2018_dslr_mounted
--- lib/scripts/take_photos_continuously.sh
+++ lib/scripts/take_photos_continuously.sh
@@ -1,2 +1,3 @@
 fusermount -u /tmp/suas_2018_dslr_mounted
-#gphoto2 --set-config capturetarget=1 --set-config drivemode=1 --set-config eosremoterelease=2 --list-config --wait-event=3s --set-config eosremoterelease=0
+#gphoto2 --force-overwrite --set-config imageformat=3  --set-config capturetarget=1 --set-config iso=0 --set-config eosremoterelease=5 --wait-event-and-download=100s --set-config eosremoterelease=4
+gphoto2 --force-overwrite --set-config imageformat=3  --set-config capturetarget=1 --set-config iso=0 --set-config eosremoterelease=5 --wait-event=100s --set-config eosremoterelease=4
