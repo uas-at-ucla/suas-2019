35677101734bb4ff7ea81a59ab77b8387920e050
Ivan Manan <ivanmanan@gmail.com>
2018-05-29 22:26:53 -0700

    Ready to be merged to master
    Former-commit-id: 9637bb871001b755166c8aff53119993606432c5

--- src/ground/interface/src/App.jsx
+++ src/ground/interface/src/App.jsx
@@ -15,7 +15,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      optionSelected: "Images", // Default is Control
+      optionSelected: "Control", // Default is Control
       droneState: "Offline",
       telemetry: null,
       interopBtnText: "Connect to Interop",
