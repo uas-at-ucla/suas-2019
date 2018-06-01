16fa6b6e84faaa7f8976d84cae661f30a90e4a6d
Ben L <ben.limpanukorn@gmail.com>
2018-06-01 13:40:17 -0700

    Sample interface implementation for cropping images

--- src/ground/interface/src/Images/Modal.jsx
+++ src/ground/interface/src/Images/Modal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import io from "socket.io-client/dist/socket.io.js";
 
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
@@ -9,11 +10,37 @@ export default class ModalComponent extends React.Component {
             display: true
         };
         this.toggle = this.toggle.bind(this);
+        this.send_manual = this.send_manual.bind(this);
+        this.send_manual_test = this.send_manual_test.bind(this);
+    }
+
+    componentWillMount() {
+        this.vision_socket = io.connect("http://" + document.domain + ":8099");
+        this.vision_socket.on("manual_request_done", data => {
+            console.log(JSON.stringify(data));
+        });
     }
 
     toggle() {
         this.setState({
             display: !this.state.display
+        });
+    }
+
+    send_manual_test() {
+        this.send_manual("4fg2E94PTjKwb681diPaGA", [25, 25], [50, 50]);
+    }
+
+    send_manual(img_id, top_left, bottom_right) {
+        this.vision_socket.emit("manual_request", {
+            'event_name': 'snip',
+            'args': {
+                'img_id': img_id,
+                'yolo_results': [{
+                    'topleft':      {'x': top_left[0], 'y': top_left[1]},
+                    'bottomright':  {'x': bottom_right[0], 'y': bottom_right[1]}
+                }]
+            }
         });
     }
 
@@ -24,7 +51,7 @@ export default class ModalComponent extends React.Component {
                 <Modal isOpen={ this.state.display } size="lg">
                     <ModalHeader>Photoshop</ModalHeader>
                     <ModalBody>
-                        <Button>Crop</Button>
+                        <Button onClick={this.send_manual_test}>Crop</Button>
                         <ModalFooter>
                             <img className="resize" src={photoURL}/>
                         </ModalFooter>
