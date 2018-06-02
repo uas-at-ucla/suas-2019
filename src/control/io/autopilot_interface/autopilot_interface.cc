0581669973e2d663791ac1e6a50251bd6b8e7023
Comran Morshed <comranmorsh@gmail.com>
2018-06-02 11:43:56 -0700

    Add stuff at field.

--- src/control/io/autopilot_interface/autopilot_interface.cc
+++ src/control/io/autopilot_interface/autopilot_interface.cc
@@ -410,6 +410,7 @@ void AutopilotInterface::set_params() {
   set_param("MIS_TAKEOFF_ALT", 5.0);
   set_param("MPC_XY_CRUISE", 10.0);
   set_param("MPC_XY_VEL_MAX", 10.0);
+  set_param("MC_YAWRAUTO_MAX", 25.0);
 }
 
 void AutopilotInterface::set_param(const char id[], float value) {
