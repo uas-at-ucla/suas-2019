971bf477696de62e36fa47596b7ccf08e99f039e
Ryan N <ryguyn@gmail.com>
2018-06-04 19:22:58 -0700

    Cropped images can be manually classified and sent to ground backend TODO: send to interop

--- src/ground/interface/src/Images/Images.jsx
+++ src/ground/interface/src/Images/Images.jsx
@@ -3,6 +3,7 @@ import './Images.css';
 import logo from '../graphics/vector_logo.svg';
 import Position from './Position';
 import ModalComponent from './Modal';
+import Classify from './Classify'
 
 import { PageHeader } from 'react-bootstrap';
 
@@ -10,8 +11,8 @@ class Images extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      doubleClicked: false,
       currentPhoto: null,
+      currentCroppedPhoto: null,
       photo_lat: null,
       photo_lon: null
     }
@@ -26,14 +27,24 @@ class Images extends Component {
             <h3>Raw Images</h3>
             <div id="Raw-Images-List">
               {this.renderRawImages()}
-              {
-                this.state.doubleClicked ? <ModalComponent image={this.state.currentPhoto}/> : null
+              {this.state.currentPhoto ? 
+                <ModalComponent image={this.state.currentPhoto} 
+                                doubleClick={this.state.doubleClickRaw}
+                                socketEmit={this.props.socketEmit}/> : null
               }
             </div>
           </div>
           <div className="col-md-4 col-sm-4 col-xs-4 text-center">
             <h3>Segmented Images</h3>
-            {this.renderSegmentedImages()}
+            <div id="Segmented-Images-List">
+              {this.renderSegmentedImages()}
+              {this.state.currentCroppedPhoto ? 
+                <Classify image={this.state.currentCroppedPhoto} 
+                          doubleClick={this.state.doubleClickCropped}
+                          socketEmit={this.props.socketEmit}
+                          object={this.state.submittedObject}/> : null
+              }
+            </div>
           </div>
           <div className="col-md-4 col-sm-4 col-xs-4 text-center">
             <h3>Position of Photo Taken</h3>
@@ -52,13 +63,6 @@ class Images extends Component {
     );
   }
 
-  changeDoubleClicked() {
-    this.setState({
-      doubleClicked: false
-    });
-    console.log(this.state.doubleClicked);
-  }
-
   renderRawImages() {
     //console.log("number of images: " + this.props.appState.rawImages.length);
     return this.props.appState.rawImages.map((photo, index) => {
@@ -67,7 +71,8 @@ class Images extends Component {
              key={index}
              id={photo.id} src={photo.src}
              onDoubleClick={() => this.photoshop(photo)}
-             onClick={()=>this.showPosition(photo)} />
+             onClick={()=>this.showPosition(photo)}
+             style={this.rawImageStyle(photo.id)} />
       );
     })
   }
@@ -78,16 +83,50 @@ class Images extends Component {
         <img className="surveyPhotoOdd"
              key={index}
              id={photo.id} src={photo.src}
-             onClick={()=>this.showPosition(photo)} />
+             onDoubleClick={() => this.classify(photo)}
+             onClick={()=>this.showPosition(photo)}
+             style={this.segmentedImageStyle(photo.id)} />
       );
     })
   }
 
+  segmentedImageStyle(id) {
+    if (this.props.appState.manualClassifiedImages.find(el => el.id === id)) {
+      return {borderBottom: "4px solid rgba(0, 255, 0, 2)"};
+    } else if (this.props.appState.manualCroppedImageParents.find(el => el === this.props.appState.croppedImages[id])) {
+      return {borderBottom: "4px solid rgba(255, 0, 0, 2)"};
+    } else {
+      return {borderBottom: "1px solid rgba(81, 203, 238, 2)"};
+    }
+  }
+
+  rawImageStyle(id) {
+    if (this.props.appState.manualClassifiedImages.find(el => this.props.appState.croppedImages[el.id] === id)) {
+      return {borderBottom: "4px solid rgba(0, 255, 0, 2)"};
+    } else if (this.props.appState.manualCroppedImageParents.find(el => el === id)) {
+      return {borderBottom: "4px solid rgba(255, 0, 0, 2)"};
+    } else {
+      return {borderBottom: "1px solid rgba(81, 203, 238, 2)"};
+    }
+  }
+
   photoshop = (photo) => {
-    console.log("toggle: " + this.state.doubleClicked);
     this.setState({
-      doubleClicked: !this.state.doubleClicked,
+      doubleClickRaw: {},
       currentPhoto: photo
+    });
+  }
+
+  classify = (photo) => {
+    let object = null;
+    let classified = this.props.appState.manualClassifiedImages.find(el => el.id === photo.id);
+    if (classified) {
+      object = classified.object;
+    }
+    this.setState({
+      doubleClickCropped: {},
+      currentCroppedPhoto: photo,
+      submittedObject: object
     });
   }
 
@@ -102,11 +141,6 @@ class Images extends Component {
           photo_lon: location_data.location.lon
         });
       });
-  }
-
-  submitToInterop = () => {
-    // todo: submit images to interop server
-    console.log("Hello!")
   }
 }
 
