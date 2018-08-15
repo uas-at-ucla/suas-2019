a6f94c747ac6276dd9120109c2d5c7755a2833e1
Comran Morshed <comran.morshed@dji.com>
2018-08-15 00:45:42 -0700

    Clean up output from simulate/kill scripts.

--- lib/scripts/docker/exec_kill.sh
+++ lib/scripts/docker/exec_kill.sh
@@ -7,4 +7,4 @@ UAS_AT_UCLA_IMAGE=$(docker ps \
   --format "{{.ID}}" \
   --latest)
 
-docker exec -t $UAS_AT_UCLA_IMAGE sh -c "for f in /tmp/docker-exec-*.pid;do PID=\$(cat \"\$f\");echo \"KILLING \$PID\";kill -15 \$PID || true;done"
+docker exec -t $UAS_AT_UCLA_IMAGE sh -c "for f in /tmp/docker-exec-*.pid;do PID=\$(cat \"\$f\");echo \"KILLING \$PID\";kill -15 \$PID > /dev/null 2>&1 || true;rm \$f;done"
