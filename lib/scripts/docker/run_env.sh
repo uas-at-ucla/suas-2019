49c2f852d278cca51b2f9d6d5239a306235b6a58
Comran Morshed <comran.morshed@dji.com>
2018-08-17 00:30:09 -0700

    Add more verbosity to building uas_env docker.
    Former-commit-id: c116ed8e97f8d00f42796b7b308b39217d4bba34 [formerly a7ee07ab82e9a1a0333149e3c691d82d13f3fbdf]
    Former-commit-id: ff28a5b20ecc6dd392c52f74672ff62f5cbf8652

--- lib/scripts/docker/run_env.sh
+++ lib/scripts/docker/run_env.sh
@@ -33,6 +33,11 @@ fi
 # Build docker container.
 docker build -t uas-at-ucla_software tools/docker
 
+if [ $? -ne 0 ]; then
+    echo "Error building UAS env docker container."
+    exit 1
+fi
+
 # Create network for docker container to use.
 docker network create -d bridge uas_bridge > /dev/null || true
 
