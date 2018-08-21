2b78f693dfc375b2f8c36c0e1aff48f1b869b64b
Comran Morshed <comran.morshed@dji.com>
2018-08-21 10:23:54 -0700

    Make travis compile for both linux and MacOS.

---
+++ tools/scripts/docker/exec_interactive.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function docker_exec {
+    unset UAS_AT_UCLA_IMAGE
+    UAS_AT_UCLA_IMAGE=$(docker ps \
+      --filter status=running \
+      --filter name=uas_env \
+      --format "{{.ID}}" \
+      --latest)
+
+    if [ -z $UAS_AT_UCLA_IMAGE ]
+    then
+      echo "Could not find uas env docker image. Exiting..."
+      exit 1
+    fi
+
+    PIDFILE=/tmp/docker-exec-$$
+    NAMEFILE=/tmp/docker-exec-$$
+
+    docker exec -it -u $(id -u) $UAS_AT_UCLA_IMAGE \
+      bash -c "echo \"\$\$\" > \"$PIDFILE\".pid; echo \"$*\" > \"$NAMEFILE\".name;$*"
+
+    exit $?
+}
+
+docker_exec "$@"
