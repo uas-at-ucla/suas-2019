fa1e0bcbb8063e94eee4af1cc9a71f938c471af6
Comran Morshed <comran.morshed@dji.com>
2018-08-24 16:03:43 -0700

    Create host machine group in docker if not exists.

--- tools/scripts/docker/run_env.sh
+++ tools/scripts/docker/run_env.sh
@@ -46,7 +46,7 @@ mkdir -p tools/docker/cache/bazel
 
 # Start docker container and let it run forever.
 PLATFORM=$(uname -s)
-DOCKER_BUILD_CMD="usermod -u $(id -u) -g $(id -g) uas;chown -R uas /home/uas/.cache/bazel;sudo -u uas bash -c \"bazel;sleep infinity\""
+DOCKER_BUILD_CMD="set -x getent group $(id -g) || groupadd -g $(id -g) host_group;usermod -u $(id -u) -g $(id -g) uas;chown -R uas /home/uas/.cache/bazel;sudo -u uas bash -c \"bazel;sleep infinity\""
 if [ "$PLATFORM" == "Darwin" -o "$TRAVIS" == "true" ]
 then
   DOCKER_BUILD_CMD="$DOCKER_BUILD_CMD"
