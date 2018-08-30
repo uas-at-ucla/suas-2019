209c529e205315f3cdcdea0e426ebd4b2c8bb0d3
Ryan Nemiroff <ryguyn@gmail.com>
2018-08-30 00:07:08 -0700

    Added script to start docker machine on Mac

---
+++ tools/scripts/docker/start_machine_mac.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ $(uname -s) == "Darwin" ]
+then
+  docker version > /dev/null 2>&1
+  if [ $? -ne 0 ]
+  then
+    UAS_ENV_MACHINE_STATUS="$(docker-machine status uas-env 2> /dev/null)"
+    if [ $? -ne 0 ]
+    then
+      echo ""
+      echo "Creating uas-env docker machine:"
+      echo "docker-machine create -d virtualbox --virtualbox-memory 4096 --virtualbox-cpu-count 3 --virtualbox-disk-size 15000 uas-env"
+      echo ""
+      docker-machine create -d virtualbox --virtualbox-memory 4096 --virtualbox-cpu-count 3 --virtualbox-disk-size 15000 uas-env
+      docker-machine-nfs uas-env --shared-folder=$(pwd) -f --nfs-config="-alldirs -mapall=$(id -u):20"
+      echo ""
+      echo "Started uas-env docker machine. To stop it, run \"docker-machine stop uas-env\""
+      echo ""
+    else
+      if [ "$UAS_ENV_MACHINE_STATUS" != "Running" ]
+      then
+        echo ""
+        echo "Starting uas-env docker machine. To stop it, run \"docker-machine stop uas-env\""
+        echo ""
+        docker-machine start uas-env
+      fi
+    fi
+    eval $(docker-machine env uas-env)
+  fi
+fi
