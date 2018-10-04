993aa6f8f6a00c434f00280f6f3b5dc13a846c60
Ben L <ben.limpanukorn@gmail.com>
2018-10-04 10:44:04 -0700

    Rsync client now downloads multiple files
    Changes ported from commit 56bd9f436877bbe413fafbcf3e93f32e3340bca8

--- src/vision/vision.py
+++ src/vision/vision.py
@@ -358,8 +358,8 @@ def process_image(json, attempts=1):
             }],
             'user': DRONE_USER,
             'addr': DRONE_IP,
-            'img_remote_src': json['file_path'],
-            'img_local_dest': img_inc_path + '.jpg'
+            'img_remote_src': [json['file_path']],
+            'img_local_dest': [img_inc_path + '.jpg']
         }
     })
     # yapf: enable
@@ -414,8 +414,8 @@ def download_snipped(json):
     download_dir = json['download_dir']
     print("Telling rsync client to download snipped image")
     # yapf: disable
-    # WARNING do not combine these using a for loop,
-    # otherwise duplicate next tasks will be sent
+    # WARNING: this auctions must occur as one event
+    # to prevent duplicate "next" calls
     server_task_queue.put({
         'type': 'auction',
         'event_name': 'rsync',
@@ -440,23 +440,10 @@ def download_snipped(json):
             ],
             'user': SNIPPER_USER,
             'addr': SNIPPER_IP,
-            'img_remote_src': os.path.join(download_dir, img_id + '.jpg'),
-            'img_local_dest': os.path.join(DEFAULT_DATA_DIR, img_id + '.jpg')
-        }
-    })
-    server_task_queue.put({
-        'type': 'auction',
-        'event_name': 'rsync',
-        'args': {
-            'prev': {
-                'event_name': 'snipped',
-                'json': json
-            },
-            'next': [],
-            'user': SNIPPER_USER,
-            'addr': SNIPPER_IP,
-            'img_remote_src': os.path.join(download_dir, img_id + '.json'),
-            'img_local_dest': os.path.join(DEFAULT_DATA_DIR, img_id + '.json')
+            'img_remote_src': [os.path.join(download_dir, img_id + ext)
+                               for ext in ('.jpg', '.json')],
+            'img_local_dest': [os.path.join(DEFAULT_DATA_DIR, img_id + ext)
+                               for ext in ('.jpg', '.json')]
         }
     })
     # yapf: enable
@@ -696,10 +683,15 @@ class RsyncWorker(ClientWorker):
             print('Called rsync with args: <' + '> <'.join(map(str, task)) +
                   '>')
 
-        if 0 == processes.spawn_process_wait_for_code(
-                'rsync -vz --progress -e "ssh -p 22" "' + task_args['user'] +
-                '@' + task_args['addr'] + ':' + task_args['img_remote_src'] +
-                '" ' + task_args['img_local_dest']):
+        success = True
+        for i in range(len(task_args['img_remote_src'])):
+            remote = task_args['img_remote_src'][i]
+            local = task_args['img_remote_dest'][i]
+            if 0 != processes.spawn_process_wait_for_code(
+                    'rsync -vz --progress -e "ssh -p 22" "' + task_args['user']
+                    + '@' + task_args['addr'] + ':' + remote + '" ' + local):
+                success = False
+        if success:
             self._emit(
                 task, 'download_complete', {
                     'saved_path': task_args['img_local_dest'],
