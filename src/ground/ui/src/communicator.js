3f75181788dc8d3fb684c1cd2758a13c2d7efb2d
Ryan Nemiroff <ryguyn@gmail.com>
2018-10-04 00:27:59 -0700

    Began adding socketio to new React app

--- src/ground/ui/src/communicator.js
+++ src/ground/ui/src/communicator.js
@@ -1,1 +1,19 @@
-communicator.js
+import socketIOClient from 'socket.io-client';
+
+socketHost = window.location.hostname; // read from browser URL
+socketPort = 8081;
+socket = socketIOClient(socketHost+':'+socketPort+'/ui', { transports: ['websocket'] });
+
+export default (store) => {
+    socket.on('telemetry', (data) => {
+        store.dispatch('TELEMETRY', data);
+    })
+
+    // middleware
+    return (store) => (next) => (action) => {
+        console.log('dispatching', action)
+        let result = next(action)
+        console.log('next state', store.getState())
+        return result
+    }
+}
