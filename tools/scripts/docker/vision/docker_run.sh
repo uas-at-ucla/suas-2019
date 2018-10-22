3c55929f2e5b26d8745c64f46e4b75d53b0d401f
Ben L <ben.limpanukorn@gmail.com>
2018-10-21 17:41:24 -0700

    Run script network fixes
    - the run script now publishes the port for the vision socketio server
    and allows the client to connect to the host network
    - also updated the script to name the running containers with
    a partially random name so that the nuke script can delete the
    containers appropriately

--- tools/scripts/docker/vision/docker_run.sh
+++ tools/scripts/docker/vision/docker_run.sh
@@ -3,6 +3,7 @@ cd $(dirname $0)
 source ../start_machine_mac.sh
 cd ../../../../src/vision
 
+# docker running on windows expects a windows-style path
 if [[ $(which wslpath 2>/dev/null) = "/bin/wslpath" ]]
 then
     true_path="$(wslpath -w $(pwd))\\data_local"
@@ -10,6 +11,18 @@ else
     true_path="$(pwd)/data_local"
 fi
 
+if [[ $1 = "client" ]]
+then
+    extra_parameters='--network=host' # allows it to connect to localhost
+else
+    extra_parameters="-p 8099:8099" # allows clients to connect
+fi
+
+# generate a random id
+vision_container_id=$(head -c 5 /dev/urandom | base32)
+
 docker run \
+    $extra_parameters \
+    --name uas-at-ucla_vision-$vision_container_id \
     --mount type=bind,source="$true_path",target=/suas/src/vision/data_local \
     uas-at-ucla_vision:latest "$@"
