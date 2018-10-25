6c194c9a37ff118a183fa6527e906daa90cc8fa5
Ryan Nemiroff <ryguyn@gmail.com>
2018-10-24 17:45:13 -0700

    ground.py no longer depends on bash (can run on Windows)

--- src/ground/ground.py
+++ src/ground/ground.py
@@ -1,28 +1,30 @@
 import os
 import sys
 import argparse
+import subprocess
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 sys.dont_write_bytecode = True
-sys.path.insert(0, '../../lib')
-import process_manager
-
-processes = process_manager.ProcessManager()
-
-def run_command(cmd):
-    processes.spawn_process_wait_for_code(cmd)
-
-# TODO: Handle command line arguments
-# print(sys.argv)
+sys.path.insert(0, 'tools')
+import npm_install
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--build', action='store_true')
+    parser.add_argument('--server', action='store_true')
+    parser.add_argument('--server', action='store_true')
     args = parser.parse_args()
 
-    run_command("cd server; ../tools/npm_install.sh")
-    run_command("cd ui; ../tools/npm_install.sh")
+    os.chdir("server")
+    npm_install.npm_install()
+    os.chdir("..")
+
+    os.chdir("ui")
+    npm_install.npm_install()
+    os.chdir("..")
 
     # Run only if the user did not specify to just build the code.
     if not args.build:
-        processes.run_command("cd server; npm start")
+        os.chdir("server")
+        subprocess.call(["npm", "start"])
+        os.chdir("..")
