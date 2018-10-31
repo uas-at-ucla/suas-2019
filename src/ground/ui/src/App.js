b9c6137a8e3df326a54f525c0e0bfd23dba34c27
Ryan Nemiroff <ryguyn@gmail.com>
2018-10-31 13:01:40 -0700

    Added tabs using react-router-dom

--- src/ground/ui/src/App.js
+++ src/ground/ui/src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import './App.css';
 import Controls from './components/Controls/Controls';
+import Vision from './components/Vision/Vision';
+import Settings from './components/Settings/Settings';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Controls/>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Controls</Link>
+                </li>
+                <li>
+                  <Link to="/vision/">Vision</Link>
+                </li>
+                <li>
+                  <Link to="/settings/">Settings</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <Route path="/" exact component={Controls} />
+            <Route path="/vision/" component={Vision} />
+            <Route path="/settings/" component={Settings} />
+          </div>
+        </Router>
       </div>
     );
   }
