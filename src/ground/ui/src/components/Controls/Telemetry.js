e11f57ee860e6b14022d57b54d7466a8d40f295b
Neil Prajapati <neilp0101@gmail.com>
2018-10-31 15:01:12 -0700

    UI socket io events to server

--- src/ground/ui/src/components/Controls/Telemetry.js
+++ src/ground/ui/src/components/Controls/Telemetry.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {test, change_drone_state, DroneStates} from "../../actions/exampleActions"
 
 const mapStateToProps = state => {
   return {
@@ -7,14 +8,45 @@ const mapStateToProps = state => {
   };
 };
 
+const mapDispatchToProps = dispatch => {
+  return {
+    onTest: () =>{
+      dispatch(test("hi"))
+    },
+    //idk if this is right bc it looks kinda extra...
+    droneTakeoff: () => {
+      dispatch(change_drone_state(DroneStates.TAKEOFF));
+    },
+    droneLand: () => {
+      dispatch(change_drone_state(DroneStates.LAND));
+    },
+    droneFailsafe: () => {
+      dispatch(change_drone_state(DroneStates.FAILSAFE));
+    },
+    droneThrottleCut: () => {
+      dispatch(change_drone_state(DroneStates.THROTTLE_CUT));
+    },
+
+  };
+};
+
 class Telemetry extends Component {
   render() {
     return (
-      <div className="Telemetry">
-        {this.props.telemetry}
+      <div>
+        <span> Change Drone State </span>
+         <button onClick = {this.props.droneTakeoff}>TAKEOFF</button>
+         <button onClick = {this.props.droneLand}>LAND</button>
+         <button onClick = {this.props.droneFailsafe}>FAILSAFE</button>
+         <button onClick = {this.props.droneThrottleCut}>THROTTLE_CUT</button>
+
+        <div className="Telemetry">
+
+          {this.props.telemetry}
+        </div>
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps)(Telemetry);
+export default connect(mapStateToProps, mapDispatchToProps)(Telemetry);
