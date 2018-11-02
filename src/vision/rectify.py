3582c55fe7bf7ccc48764309e06c41e13f144711
Eric Wang <eric.zhexuan.wang@gmail.com>
2018-11-02 14:51:58 -0700

    rectifies off axis image

---
+++ src/vision/rectify.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import numpy
+import math
+
+#order: topleft, topright, botright, botleft
+#transforms so that pb goes to pa
+def find_coeffs(pa, pb):
+	matrix = []
+	for p1, p2 in zip(pa, pb):
+		matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0]*p1[0], -p2[0]*p1[1]])
+		matrix.append([0, 0, 0, p1[0], p1[1], 1, -p2[1]*p1[0], -p2[1]*p1[1]])
+
+	A = numpy.matrix(matrix, dtype=numpy.float)
+	B = numpy.array(pb).reshape(8)
+
+	res = numpy.dot(numpy.linalg.inv(A.T * A) * A.T, B)
+	return numpy.array(res).reshape(8)
+
+#angles in degrees xD
+def rectify(src, dest, angleOfDepression, verticalFieldOfView):
+	img = Image.open(src)
+	w,h = img.size
+	pi = math.pi
+	a = verticalFieldOfView*pi/180
+	b = (angleOfDepression-(verticalFieldOfView/2))*pi/180
+
+	w1 = int(w*math.sin(a+b)/math.cos((pi/2.0)-a))
+	h1 = int(h*(math.sin(a+b)*math.tan((pi/2.0)-a)-math.cos(a+b))/math.sqrt(2-2*math.cos(b)))
+
+	#pa = [(0,0),(w1,0),((w+w1)/2,h1),((w1-w)/2,h1)]
+	pa = [((w-w1)/2,0),((w+w1)/2,0),(w,h1),(0,h1)]
+	pb = [(0,0),(w,0),(w,h),(0,h)]
+
+	coeffs = find_coeffs(pa,pb)
+	img.transform((w,h1), Image.PERSPECTIVE, coeffs, Image.BICUBIC).save(dest)
+	#img.transform((w1, h1), Image.PERSPECTIVE, coeffs, Image.BICUBIC).save(dest)
