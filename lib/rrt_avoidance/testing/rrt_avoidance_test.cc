c4b8118bba2877c9525e54f0ded554de9f04e4cf
weberchichi@gmail.com <chiliwei759@gmail.com>
2018-11-03 14:18:34 -0700

    add multiple random obstacle test

--- lib/rrt_avoidance/testing/rrt_avoidance_test.cc
+++ lib/rrt_avoidance/testing/rrt_avoidance_test.cc
@@ -13,6 +13,45 @@ namespace testing {
 
 bool plot = false;
 
+void draw_obstacle(::lib::mission_manager::StaticObstacle *obstacle, ::std::vector<double> *obs_x, ::std::vector<double> *obs_y) {
+    const double coordinate_to_meter = GetDistance2D({0, 0, 0}, {1, 0, 0});
+    // Draw circular obstacles on plot.
+    double coord_radius = obstacle->cylinder_radius() / coordinate_to_meter;
+    ::lib::mission_manager::Position2D *obstacle_position =
+        obstacle->mutable_location();
+ 
+    for (double x = obstacle_position->latitude() - coord_radius;
+         x < obstacle_position->latitude() + coord_radius;
+         x += coord_radius / 20) {
+      double y_max = sqrt(pow(coord_radius, 2) -
+                          pow(x - obstacle_position->latitude(), 2));
+
+      for (double y = obstacle_position->longitude() - y_max;
+           y < obstacle_position->longitude() + y_max; y += coord_radius / 20) {
+        obs_x->push_back(x);
+        obs_y->push_back(y);
+      }
+    }
+}
+
+void add_obstacle(::lib::mission_manager::Obstacles *obstacles, double longitude, double latitude, double radius, bool draw=false) {
+    ::std::cout << "adding obstacle: (" << longitude << ", " << latitude << ", " << radius << ")" << ::std::endl;
+    ::lib::mission_manager::StaticObstacle *obstacle =
+      obstacles->add_static_obstacles();
+    ::lib::mission_manager::Position2D *obstacle_position =
+      obstacle->mutable_location();
+    obstacle_position->set_latitude(latitude);
+    obstacle_position->set_longitude(longitude);
+    obstacle->set_cylinder_radius(radius);
+
+    if (draw) {
+        ::std::cout << "draw" << ::std::endl;
+        ::std::vector<double> obs_x, obs_y;
+        draw_obstacle(obstacle, &obs_x, &obs_y);
+        ::matplotlibcpp::plot(obs_x, obs_y);
+    }   
+}
+
 TEST(RRTAvoidance, NoObstacles) {
   // Check that RRT takes a straight line path from position to goal.
 
@@ -53,18 +92,13 @@ TEST(RRTAvoidance, DodgeObstacles) {
   for (int calcs = 0; calcs < 10; calcs++) {
     ::std::cout << "Running calculation #" << calcs << ::std::endl;
 
-    Position3D start = {3e-3, 3e-3, 0};
+    Position3D start = {4e-3, 4e-3, 0};
     Position3D end = {1e-3, 1e-3, 0};
     ::lib::mission_manager::Obstacles obstacles;
-
-    for (int i = 0; i < 2; i++) {
-      ::lib::mission_manager::StaticObstacle *obstacle =
-          obstacles.add_static_obstacles();
-      ::lib::mission_manager::Position2D *obstacle_position =
-          obstacle->mutable_location();
-      obstacle_position->set_latitude(2e-3);
-      obstacle_position->set_longitude(1.6e-3 + i * 1.2e-3);
-      obstacle->set_cylinder_radius(50);
+    double testcases[][3] = {{2e-3, 2e-3, 50}, {3e-3, 3e-3, 50}};
+    int i;
+    for (i = 0; i < sizeof(testcases)/sizeof(testcases[0]); i++) {
+    	add_obstacle(&obstacles, testcases[i][0], testcases[i][1], testcases[i][2]);
     }
 
     ::std::vector<Position3D> avoidance_path =
@@ -127,6 +161,8 @@ TEST(RRTAvoidance, DodgeObstacles) {
   }
 }
 
+
+
 // TODO(comran): Make this test not freeze after performing only a couple
 // calculations.
 TEST(RRTAvoidance, RandomObstacle) {
@@ -135,13 +171,19 @@ TEST(RRTAvoidance, RandomObstacle) {
 
   RRTAvoidance rrt_avoidance;
   const double coordinate_to_meter = GetDistance2D({0, 0, 0}, {1, 0, 0});
+	
+    ::std::cout << "coordinate_to_meter: " << coordinate_to_meter << ::std::endl; 
+    double end_coord_long = 1e3;
+    double end_coord_lat = 1e3;
+    Position3D start = {0.0, 0.0, 0};
+    Position3D end = {end_coord_long / coordinate_to_meter, end_coord_lat / coordinate_to_meter, 0};
 
   for (int calcs = 0; calcs < 20; calcs++) {
     ::std::cout << "Running calculation #" << calcs << ::std::endl;
 
-    Position3D start = {0.0, 0.0, 0};
-    Position3D end = {1e3 / coordinate_to_meter, 1e3 / coordinate_to_meter, 0};
     ::lib::mission_manager::Obstacles obstacles;
+    // generate testcases evenly in longitude and latitude
+    
     ::lib::mission_manager::StaticObstacle *obstacle =
         obstacles.add_static_obstacles();
     ::lib::mission_manager::Position2D *obstacle_position =
@@ -154,21 +196,6 @@ TEST(RRTAvoidance, RandomObstacle) {
                                     coordinate_to_meter);
     obstacle_position->set_longitude((300 + 400 * rand_point) /
                                      coordinate_to_meter);
-
-    // Draw circular obstacles on plot.
-    double coord_radius = obstacle->cylinder_radius() / coordinate_to_meter;
-    for (double x = obstacle_position->latitude() - coord_radius;
-         x < obstacle_position->latitude() + coord_radius;
-         x += coord_radius / 20) {
-      double y_max = sqrt(pow(coord_radius, 2) -
-                          pow(x - obstacle_position->latitude(), 2));
-
-      for (double y = obstacle_position->longitude() - y_max;
-           y < obstacle_position->longitude() + y_max; y += coord_radius / 20) {
-        obs_x.push_back(x);
-        obs_y.push_back(y);
-      }
-    }
 
     ::std::vector<Position3D> avoidance_path =
         rrt_avoidance.Process(start, end, obstacles);
@@ -190,12 +217,12 @@ TEST(RRTAvoidance, RandomObstacle) {
     ASSERT_LE(avoidance_path[avoidance_path.size() - 1].longitude,
               end.longitude + tolerance);
 
-    //  if (plot) {
+    //if (plot) {
     //    ::matplotlibcpp::xkcd();
     //    ::matplotlibcpp::plot(final_x, final_y);
     //    ::matplotlibcpp::plot(obs_x, obs_y);
     //    ::matplotlibcpp::show();
-    //  }
+    //}
   }
 }
 
@@ -244,6 +271,74 @@ TEST(RRTAvoidance, RealObstacles) {
     ::matplotlibcpp::plot(final_x, final_y);
     ::matplotlibcpp::show();
   }
+}
+
+
+
+TEST(RRTAvoidance, MultipleRandomObstacles) {
+    const double coordinate_to_meter = GetDistance2D({0, 0, 0}, {1, 0, 0});
+    
+    double end_coord_long = 1e3;
+    double end_coord_lat = 1e3;
+    Position3D start = {0.0, 0.0, 0};
+    Position3D end = {end_coord_long / coordinate_to_meter, end_coord_lat / coordinate_to_meter, 0};
+    // density = # of obstacles/distance spanned 
+    double density = 0.01;
+    int num_long_obstacles = (int) (end_coord_long*density);
+    int num_lat_obstacles = (int) (end_coord_lat*density);
+
+    ::std::vector<::std::tuple<double, double, int>> testcases;
+    int i;
+    double lo;
+
+    ::std::cout << "num_long_obstacles: " << num_long_obstacles << ::std::endl;
+    // generate longitude obstacles
+    for (i = 0, lo=0; i < num_long_obstacles; i++, lo=(i+1)*(int)(1/density)) {
+	// generate random longitude coordinates
+	double rand_lat = double(rand()%(int)end_coord_lat);
+	int rand_rad = rand()%30+1;
+	int rand_show = rand()%2;
+	if (rand_show == 1) {
+	    testcases.emplace_back(lo, rand_lat, rand_rad);
+	    ::std::cout << lo << ", " 
+			<< rand_lat << ", " 
+			<< rand_rad << ", "
+			<< ::std::endl;
+	    }
+    }
+
+    if (plot) {
+        ::matplotlibcpp::xkcd();
+    }
+    ::lib::mission_manager::Obstacles obstacles;
+    
+    ::std::cout << "num: " << testcases.size() << ::std::endl;
+    for (i = 0; i < testcases.size(); i++) {
+        add_obstacle(&obstacles,
+                    ::std::get<0>(testcases[i])/coordinate_to_meter,
+                    ::std::get<1>(testcases[i])/coordinate_to_meter,
+                    ::std::get<2>(testcases[i]),
+                    true);
+    }
+
+    RRTAvoidance rrt_avoidance;
+ 
+    ::std::vector<Position3D> avoidance_path =
+      rrt_avoidance.Process(start, end, obstacles);
+
+    ::std::vector<double> final_x, final_y;
+
+    for (size_t i = 0; i < avoidance_path.size(); i++) {
+      ::std::cout << avoidance_path[i].latitude << ", "
+                  << avoidance_path[i].longitude << ::std::endl;
+
+      final_x.push_back(avoidance_path[i].latitude);
+      final_y.push_back(avoidance_path[i].longitude);
+    }
+    if (plot) {
+        ::matplotlibcpp::plot(final_x, final_y);
+        ::matplotlibcpp::show();
+    }
 }
 
 } // namespace testing
