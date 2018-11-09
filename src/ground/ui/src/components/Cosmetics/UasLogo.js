a04380eac12b507bfe1d335f80c6d7ca8402346b
fusionforscience <fn.matthew@outlook.com>
2018-11-08 19:20:15 -0800

    Added CSS to make UAS logo prettier and in a better place

--- src/ground/ui/src/components/Cosmetics/UasLogo.js
+++ src/ground/ui/src/components/Cosmetics/UasLogo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Logo from "./../../graphics/uasLogo.svg";
+import "./cosmetics.css";
 
 
 class UasLogo extends React.Component {
     render() {
         return (
-            <img src={Logo}/>
+            <div className="component-background">
+                <img className="logo-img" src={Logo}/>
+            </div>
         );
     }
 }
