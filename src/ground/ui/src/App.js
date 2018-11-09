d813c54d18ea4105d14fa915b45eb88db44c51c6
Ryan Nemiroff <ryguyn@gmail.com>
2018-11-08 17:35:29 -0800

    Added Vision.js and Settings.js component files

--- src/ground/ui/src/App.js
+++ src/ground/ui/src/App.js
@@ -1,54 +1,37 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import './App.css';
-
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-
 import Controls from './components/Controls/Controls';
+import Vision from './components/Vision/Vision';
+import Settings from './components/Settings/Settings';
 
-const Home = () => <h2>Home</h2>;
-const Vision = () => <h2>Vision</h2>;
-const Settings = () => <h2>Settings</h2>;
-
-const AppRouter = () => (
-  <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to = "/"> HOME </Link>
-          </li>
-          <li>
-            <Link to = "/controls">CONTROLS</Link>
-          </li>
-          <li>
-            <Link to = "/vision">VISION</Link>
-          </li>
-          <li>
-            <Link to = "/settings">SETTINGS</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Route path = "/" exact component = {Home} />
-      <Route path = "/controls" component = {Controls} />
-      <Route path = "/vision" component = {Vision} />
-      <Route path = "/settings" component = {Settings} />
-
-    </div>
-  </Router>
-  
-);
-
-export default AppRouter;
-
-/*class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Controls/>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to = "/">CONTROLS</Link>
+              </li>
+              <li>
+                <Link to = "/vision">VISION</Link>
+              </li>
+              <li>
+                <Link to = "/settings">SETTINGS</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path = "/" exact component = {Controls} />
+          <Route path = "/vision" component = {Vision} />
+          <Route path = "/settings" component = {Settings} />
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;*/
+export default App;
