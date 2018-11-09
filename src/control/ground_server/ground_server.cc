77033b07111d78a7fc5adbf1761ef58ec2611a5a
Comran Morshed <comranmorsh@gmail.com>
2018-11-08 16:10:27 -0800

    Add initial ground_server structure.

---
+++ src/control/ground_server/ground_server.cc
@@ -0,0 +1,12 @@
+#include "ground_server.h"
+
+namespace src {
+namespace control {
+namespace ground_server {
+
+GroundServer::GroundServer() {
+}
+
+} // namespace ground_communicator
+} // namespace control
+} // namespace src
