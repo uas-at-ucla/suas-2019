4504b5dd9281f9ae67289cded88908c20a14e33a
Ryan Nemiroff <ryguyn@gmail.com>
2018-11-09 17:48:18 -0800

    Added Altimeter and AttitudeIndicator components

---
+++ src/ground/ui/src/components/Controls/Telemetry/AttitudeIndicator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AttitudeIndicator extends Component {
+  render() {
+    return (
+      <div className="AttitudeIndicator">
+        AttitudeIndicator
+      </div>
+    );
+  }
+}
+
+export default AttitudeIndicator;
