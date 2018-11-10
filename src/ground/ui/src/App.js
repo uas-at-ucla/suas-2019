743831f25cd024431c6c16835c6ada9c9be7276c
Ryan Nemiroff <ryguyn@gmail.com>
2018-11-09 17:27:30 -0800

    Default page can be easily configured. Added Analytics page.

--- src/ground/ui/src/App.js
+++ src/ground/ui/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 
 import './App.css';
 import Controls from './components/Controls/Controls';
 import Vision from './components/Vision/Vision';
+import Analytics from './components/Analytics/Analytics';
 import Settings from './components/Settings/Settings';
+
+const defaultPage = "/controls" // "/vision"
+const DefaultPage = () => <Redirect to={defaultPage}/> 
 
 class App extends Component {
   render() {
@@ -14,20 +18,25 @@ class App extends Component {
           <nav>
             <ul>
               <li>
-                <Link to = "/">CONTROLS</Link>
+                <Link to="/controls">CONTROLS</Link>
               </li>
               <li>
-                <Link to = "/vision">VISION</Link>
+                <Link to="/vision">VISION</Link>
               </li>
               <li>
-                <Link to = "/settings">SETTINGS</Link>
+                <Link to="/analytics">ANALYTICS</Link>
+              </li>
+              <li>
+                <Link to="/settings">SETTINGS</Link>
               </li>
             </ul>
           </nav>
 
-          <Route path = "/" exact component = {Controls} />
-          <Route path = "/vision" component = {Vision} />
-          <Route path = "/settings" component = {Settings} />
+          <Route exact path="/" component={DefaultPage} />
+          <Route path="/controls" component={Controls} />
+          <Route path="/vision" component={Vision} />
+          <Route path="/analytics" component={Analytics} />
+          <Route path="/settings" component={Settings} />
         </div>
       </Router>
     );
