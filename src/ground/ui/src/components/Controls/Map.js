802e377cf991be4c1b27c8419f8bf3b5fb3303bb
Armaan Singh <armaansingh@Armaans-MacBook-Pro-2.local>
2018-11-15 19:39:15 -0800

    Added Map stuff

--- src/ground/ui/src/components/Controls/Map.js
+++ src/ground/ui/src/components/Controls/Map.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
-import { Marker } from 'react-google-maps';
+import { Marker, Polyline } from 'react-google-maps';
+import { Circle } from 'react-google-maps';
 
 import GoogleMap from '../Utils/GoogleMap/GoogleMap';
 
+
 class Map extends Component {
+  state = {
+    commands: [
+      {
+        latitude: 34.068528,
+        longitude: -118.442990,
+        type: "Waypoint",
+        name: "Waypoint 1",
+      },
+      {
+        latitude: 37.795601, 
+        longitude: -121.964005,
+        type: "Waypoint",
+        name: "Waypoint 2",
+      }
+    ]
+  }
+
+  pathCoordinates = [
+    { lat: this.state.commands[0].latitude, lng: this.state.commands[0].longitude },
+    { lat: this.state.commands[1].latitude, lng: this.state.commands[1].longitude }
+  ]
+
   render() {
     return (
       <div className="Map">
         <GoogleMap
-          defaultZoom={8}
-          defaultCenter={{ lat: -34.397, lng: 150.644 }}
+          defaultZoom={6}
+          defaultCenter={{ lat: this.state.commands[0].latitude, lng: this.state.commands[0].longitude }}
           defaultMapTypeId="satellite"
           defaultOptions={{
             disableDefaultUI: true,
@@ -17,7 +41,19 @@ class Map extends Component {
             scaleControl: true
           }}
         >
-          <Marker position={{ lat: -34.397, lng: 150.644 }} />
+          <Marker position={{ lat: this.state.commands[0].latitude, lng: this.state.commands[0].longitude }} />
+          <Marker position={{ lat: this.state.commands[1].latitude, lng: this.state.commands[1].longitude }} />
+          <Polyline path={this.pathCoordinates} 
+                    options={{strokeColor: 'red', strokeWeight: 3}} 
+                    visible={true} 
+                    draggable={false}
+                    
+          />
+          <Circle defaultCenter={this.pathCoordinates[0]}
+                  radius={200000}
+                  options ={{strokeColor: 'cyan', fillColor: 'blue'}}
+                  draggable={true}
+          />
         </GoogleMap>
       </div>
     );
