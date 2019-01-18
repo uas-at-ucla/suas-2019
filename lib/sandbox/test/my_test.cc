7ef074c42c250ffe5a4b12872320bafb36c904a7
Comran Morshed <comranmorsh@gmail.com>
2019-01-17 16:21:05 -0800

    Lint

--- lib/sandbox/test/my_test.cc
+++ lib/sandbox/test/my_test.cc
@@ -1,13 +1,14 @@
-#include <ros/ros.h>
 #include <ros/console.h>
+#include <ros/ros.h>
 
 int main(int argc, char **argv) {
   ::ros::init(argc, argv, "my_test");
   ::ros::start();
   ::ros::Rate loop(100);
-  
-  for(int i = 0;i < 1000;i++) {
+
+  for (int i = 0; i < 1000; i++) {
     loop.sleep();
-    ROS_INFO_STREAM("Hello " << "World");
+    ROS_INFO_STREAM("Hello "
+                    << "World");
   }
 }
