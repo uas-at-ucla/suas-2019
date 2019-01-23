80b88b128b61f1261b28f0868c952239d8cbfa06
Comran Morshed <comranmorsh@gmail.com>
2019-01-23 15:26:10 -0800

    Add ROS rqt visualization tool.

--- lib/sandbox/test/my_test.cc
+++ lib/sandbox/test/my_test.cc
@@ -1,16 +1,23 @@
+#include "std_msgs/String.h"
 #include <ros/console.h>
 #include <ros/ros.h>
 
 int main(int argc, char **argv) {
   ::ros::init(argc, argv, "my_test");
   ::ros::start();
-  ::ros::Rate loop(10);
+  ::ros::Rate loop(100);
 
-  for (int i = 0; i < 100; i++) {
+  ::ros::NodeHandle n;
+  ::ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  for (int i = 0; i < 10000 && ::ros::ok(); i++) {
     ::std::cout << "Here " << i << ::std::endl;
     loop.sleep();
     ROS_INFO_STREAM("Hello "
                     << "World " << i);
+
+    std_msgs::String msg;
+    msg.data = "test";
+    chatter_pub.publish(msg);
   }
 
   return 0;
