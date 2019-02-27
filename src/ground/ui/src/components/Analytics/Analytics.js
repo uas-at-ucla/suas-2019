5e3e98a6f0de21c7d28982e67c41e67d9dd0b294
Ryan Nemiroff <ryguyn@gmail.com>
2019-02-26 19:49:37 -0800

    Outlined some Analytics stuff

--- src/ground/ui/src/components/Analytics/Analytics.js
+++ src/ground/ui/src/components/Analytics/Analytics.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import './Analytics.css';
+import downloadToBrowser from '../../utils/downloadToBrowser';
+
+const mapStateToProps = state => {
+  return {
+    telemetry: state.telemetry
+  };
+};
+
+const mapDispatchToProps = {}; //TODO Make action for recording telemetry
 
 class Analytics extends Component {
   render() {
     return (
       <div className="Analytics">
+        Telemetry: {JSON.stringify(this.props.telemetry)}
+        <button>
+          Record!!! / Stop Recording (and save to file)!!
+        </button>
+        <button>
+          Load Telemetry File / Unload File
+        </button>
+        <button>
+          Run Loaded Telemetry / Pause Telemetry
+        </button>
       </div>
     );
   }
+
+  downloadTelemetry() {
+    downloadToBrowser("telemetry.json", "test content");
+  }
 }
 
-export default Analytics;
+export default connect(mapStateToProps, mapDispatchToProps)(Analytics);
