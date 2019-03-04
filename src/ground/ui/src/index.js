4ce0db50213e7e5061d6d3c8ff32d042a507076f
Ryan Nemiroff <ryguyn@gmail.com>
2019-03-04 01:12:57 -0800

    Use maps online by default

--- src/ground/ui/src/index.js
+++ src/ground/ui/src/index.js
@@ -5,8 +5,6 @@ import registerServiceWorker from './registerServiceWorker';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import 'google_maps_js_api';
-
 import './index.css';
 import App from './App';
 
