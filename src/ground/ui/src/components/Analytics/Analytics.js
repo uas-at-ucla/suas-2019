fe0cfa13b328d08372db02d42e161859b0dc1550
Ryan Nemiroff <ryguyn@gmail.com>
2019-03-05 18:32:14 -0800

    Added more buttons to Analytics

--- src/ground/ui/src/components/Analytics/Analytics.js
+++ src/ground/ui/src/components/Analytics/Analytics.js
@@ -11,27 +11,47 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {}; //TODO Make action for recording telemetry
-
 class Analytics extends Component {
   render() {
     return (
       <div className="Analytics">
         Telemetry: {JSON.stringify(this.props.telemetry)}
-        <button>
-          Record!!! / Stop Recording (and save to file)!!
-        </button>
-        <button>
-          Load Telemetry File / Unload File
-        </button>
-        <button>
-          Run Loaded Telemetry / Pause Telemetry
-        </button>
+        <div>
+          <button>
+            Record!!! / Stop Recording (and save to file)!!
+          </button>
+          <button>
+            Load Telemetry File / Unload File
+          </button>
+          <button>
+            Run Loaded Telemetry / Pause Telemetry
+          </button>
+        </div>
+
+        <div>
+          <button>
+            Save Current Interop Mission
+          </button>
+          <button>
+            Load Interop Mission / Unload File
+          </button>
+        </div>
       </div>
     );
   }
 
+  saveInteropMission() {
+    //TODO: add the interop data to mapStateToProps (see Map.js)
+    //Save it to a file using JSON.stringify and downloadToBrowser
+  }
+
+  loadInteropMission() {
+    //TODO load JSON data from a file
+    this.props.dispatch({ type: 'INTEROP_DATA', payload: data });
+  }
+
   downloadTelemetry() {
-    downloadToBrowser("telemetry.json", "test content");
+    downloadToBrowser("telemetry.json", "file text content");
   }
 }
 
