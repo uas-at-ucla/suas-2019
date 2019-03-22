eaa56a498cd7d2fa58b57dc5779fdfa011499a55
Ben L <ben.limpanukorn@gmail.com>
2019-03-22 01:17:43 -0700

    Fixes map overlays and layout

--- src/ground/ui/src/components/Vision/Vision.js
+++ src/ground/ui/src/components/Vision/Vision.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
 import './Vision.css';
 import Map from './Map/Map';
 import Tagging from './Tagging/Tagging';
 import Pipeline from './Pipeline/Pipeline';
 
+const mapStateToProps = state => {return {vision: state.vision};};
+const mapDispatchToProps = state => {};
+
 class Vision extends Component {
   render() {
     return (
       <div className="Vision">
-        <div className = "Map">
-          <Map />
+        <div className="map-overlay">
+        <Tagging />
         </div>
+        <Map />
       </div>
     );
   }
 }
 
-export default Vision;
+export default connect(mapStateToProps, mapDispatchToProps)(Vision);
