910403cd450dd9078f2810f265f0a5c425be982e
Jonathan Wong <jonathanwong113@gmail.com>
2019-04-16 17:55:48 -0700

    fixed import issues

--- dronedb/db/urls.py
+++ dronedb/db/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url, include
-from rest_framework import format_suffix_patterns
-import .views
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import CreateData, CreateShape, CreateColor, CreateCharacter, CreateOrientation
 
 
 urlpatterns={
-    url(r'^data/$', Data.as_view(), name="data")
-    url(r'^shape/$', Shape.as_view(), name="shape")
-    url(r'^char/$', Character.as_view(), name="char")
-    url(r'^orientation/$', Orientation.as_view(), name="orient")
-    url(r'^color/$', Color.as_view(), name="color")
+    url(r'^data/$', CreateData.as_view(), name="data"),
+    url(r'^shape/$', CreateShape.as_view(), name="shape"),
+    url(r'^char/$', CreateCharacter.as_view(), name="char"),
+    url(r'^orientation/$', CreateOrientation.as_view(), name="orient"),
+    url(r'^color/$', CreateColor.as_view(), name="color"),
 
 }
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- dronedb/uasdb/urls.py
+++ dronedb/uasdb/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import url,include, path
+from django.conf.urls import url, include
+
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/',include('db.urls')),
+    url('admin/', admin.site.urls),
+    url('api/',include('db.urls')),
 ]
