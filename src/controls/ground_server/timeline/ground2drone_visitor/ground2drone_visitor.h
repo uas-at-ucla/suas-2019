ea2dd838f9d332a00801c78585383d6c41fb10e6
Comran Morshed <comranmorsh@gmail.com>
2019-05-02 16:00:32 -0700

    Linting.

--- src/controls/ground_server/timeline/ground2drone_visitor/ground2drone_visitor.h
+++ src/controls/ground_server/timeline/ground2drone_visitor/ground2drone_visitor.h
@@ -15,23 +15,23 @@ namespace ground2drone_visitor {
 class Ground2DroneVisitor {
  public:
   Ground2DroneVisitor();
-  bool Process(GroundProgram *input_program, DroneProgram& drone_program);
+  bool Process(GroundProgram *input_program, DroneProgram &drone_program);
 
  private:
   void ConcatenateDroneProgramCommands(DroneProgram &base_program,
                                        DroneProgram new_program);
 
   // Drone language visitors.
-  bool Visit(GroundProgram *n, DroneProgram& drone_program);
-  bool Visit(GroundCommand *n, DroneProgram& drone_program);
-  bool Visit(WaypointCommand *n, DroneProgram& drone_program);
-  bool Visit(UgvDropCommand *n, DroneProgram& drone_program);
-  bool Visit(SurveyCommand *n, DroneProgram& drone_program);
-  bool Visit(OffAxisCommand *n, DroneProgram& drone_program);
-  bool Visit(WaitCommand *n, DroneProgram& drone_program);
+  bool Visit(GroundProgram *n, DroneProgram &drone_program);
+  bool Visit(GroundCommand *n, DroneProgram &drone_program);
+  bool Visit(WaypointCommand *n, DroneProgram &drone_program);
+  bool Visit(UgvDropCommand *n, DroneProgram &drone_program);
+  bool Visit(SurveyCommand *n, DroneProgram &drone_program);
+  bool Visit(OffAxisCommand *n, DroneProgram &drone_program);
+  bool Visit(WaitCommand *n, DroneProgram &drone_program);
 
   // Intermediate language visitors.
-  bool Visit(GotoCommand *n, DroneProgram& drone_program);
+  bool Visit(GotoCommand *n, DroneProgram &drone_program);
 };
 
 } // namespace ground2drone_visitor
