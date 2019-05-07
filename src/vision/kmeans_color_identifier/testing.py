1cbfca3214445a23b752d5abcde54601acdeba6e
andrewschechter <schechter@g.ucla.edu>
2019-05-07 12:43:06 -0700

    added kmeans stuff

---
+++ src/vision/kmeans_color_identifier/testing.py
@@ -0,0 +1,13 @@
+from os import listdir, path, system
+
+dir = 'images/test2/'
+
+for image in listdir(dir):
+    name = path.splitext(image)[0]
+    extension = path.splitext(image)[1]
+    
+    filename = dir + name + extension
+    
+    if (extension == '.jpg'):
+        print(filename)
+        system("python3 identify_colors.py " + filename)
