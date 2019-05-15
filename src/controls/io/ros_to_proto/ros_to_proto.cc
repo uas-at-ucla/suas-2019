c96aa043db36776ee58e976b570cc995d34041fb
Comran Morshed <comranmorsh@gmail.com>
2019-05-15 08:41:08 -0700

    Lint.

--- src/controls/io/ros_to_proto/ros_to_proto.cc
+++ src/controls/io/ros_to_proto/ros_to_proto.cc
@@ -235,24 +235,26 @@ void RosToProto::GotRosMessage(::std::string ros_topic) {
       ::lib::phased_loop::GetCurrentTime();
 }
 
-// from https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
-void toEulerAngle(const ::geometry_msgs::Quaternion& q, double& roll, double& pitch, double& yaw) {
-	// roll (x-axis rotation)
-	double sinr_cosp = +2.0 * (q.w * q.x + q.y * q.z);
-	double cosr_cosp = +1.0 - 2.0 * (q.x * q.x + q.y * q.y);
-	roll = atan2(sinr_cosp, cosr_cosp);
+// from
+// https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+void toEulerAngle(const ::geometry_msgs::Quaternion &q, double &roll,
+                  double &pitch, double &yaw) {
+  // roll (x-axis rotation)
+  double sinr_cosp = +2.0 * (q.w * q.x + q.y * q.z);
+  double cosr_cosp = +1.0 - 2.0 * (q.x * q.x + q.y * q.y);
+  roll = atan2(sinr_cosp, cosr_cosp);
 
-	// pitch (y-axis rotation)
-	double sinp = +2.0 * (q.w * q.y - q.z * q.x);
-	if (fabs(sinp) >= 1)
-		pitch = copysign(M_PI / 2, sinp); // use 90 degrees if out of range
-	else
-		pitch = asin(sinp);
+  // pitch (y-axis rotation)
+  double sinp = +2.0 * (q.w * q.y - q.z * q.x);
+  if (fabs(sinp) >= 1)
+    pitch = copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+  else
+    pitch = asin(sinp);
 
-	// yaw (z-axis rotation)
-	double siny_cosp = +2.0 * (q.w * q.z + q.x * q.y);
-	double cosy_cosp = +1.0 - 2.0 * (q.y * q.y + q.z * q.z);  
-	yaw = atan2(siny_cosp, cosy_cosp);
+  // yaw (z-axis rotation)
+  double siny_cosp = +2.0 * (q.w * q.z + q.x * q.y);
+  double cosy_cosp = +1.0 - 2.0 * (q.y * q.y + q.z * q.z);
+  yaw = atan2(siny_cosp, cosy_cosp);
 }
 
 } // namespace ros_to_proto
