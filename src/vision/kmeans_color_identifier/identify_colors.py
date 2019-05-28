221095f7d6c3fa81a36688f7b18bbf0528858f38
Andy Peng <andypg20@gmail.com>
2019-05-28 16:46:02 -0700

    Added really janky saving masks to file

--- src/vision/kmeans_color_identifier/identify_colors.py
+++ src/vision/kmeans_color_identifier/identify_colors.py
@@ -1,13 +1,11 @@
-
+import os
 import cv2
 import numpy as np
 import argparse
 from sklearn.cluster import KMeans
 
-
 # our colors: might need tweaking
 COLORS = [
-
     (255, 255, 255, 'white'),
     (35, 35, 35, 'black'),
     (220, 30, 30, 'red'),
@@ -21,7 +19,6 @@ COLORS = [
 ]
 
 def create_histogram(cluster):
-    
     num_labels = np.arange(0, len(np.unique(cluster.labels_)) + 1)
     hist, _ = np.histogram(cluster.labels_, bins=num_labels)
     hist = hist.astype('float32')
@@ -29,13 +26,10 @@ def create_histogram(cluster):
     return hist
 
 def extract_target(image):
-
     raw_image = image
-
     mask = np.zeros(image.shape[:2], np.uint8)
-
-    bgdModel = np.zeros((1,65),np.float64)
-    fgdModel = np.zeros((1, 65), np.float64)
+    bgdModel = np.zeros((1,65), np.float64)
+    fgdModel = np.zeros((1,65), np.float64)
 
     h, w = image.shape[:2]    
     rect = (10, 10, w-60, h-60)
@@ -44,7 +38,6 @@ def extract_target(image):
     applied_mask = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
     image = image * applied_mask[:,:, np.newaxis]
     image = cv2.bitwise_and(raw_image, image)
-
     return image
 
 
@@ -64,61 +57,58 @@ parser.add_argument("image_path")
 args = parser.parse_args()
 
 image_path = args.image_path
-
 raw_image = cv2.imread(image_path)
 height, width, _ = np.shape(raw_image)
 
 # use a foreground extraction tool to mask everything around the target
 image = extract_target(raw_image)
-
 # save a copy of this stage cause it looks cool
 masked_image = image
-
 # reshape the image to be a simple list of RGB pixels
 image = image.reshape((height * width, 3))
 
 # pick the three most common colors (one of which will be the black mask)
 clt = KMeans(n_clusters=3)
 clt.fit(image)
-
 # remove pure black (the mask) from the clusters
 clt.cluster_centers_ = clt.cluster_centers_.astype(int)
 clt.cluster_centers_ = clt.cluster_centers_[~np.all(clt.cluster_centers_ == 0, axis=1)]
-
 # create a histogram of color frequency
 histogram = create_histogram(clt)
 
 # remove the most common color (should be the black mask), but this is kinda jank
 max_index = np.where(histogram == np.amax(histogram))
 histogram = np.delete(histogram, max_index)
-
 print(histogram)
-
 # associate the histogram results with the cluster centers
 combined = zip(histogram, clt.cluster_centers_)
-
 # sort by historgram value (most dominant color)
 combined = sorted(combined, key=lambda x: x[0])
 
 for index, rows in enumerate(combined):
-
     color = rows[1]
     red, green, blue = int(color[2]), int(color[1]), int(color[0])
     rgb = (red, green, blue)
 
     name = name_color(rgb)
-
     if index:
         print("Shape Color: {}, {}".format(name, rgb))
     else:
         print("Letter Color: {}, {}".format(name, rgb))
 
+# Really hacky way to save images
+os.makedirs('output', exist_ok=True)
+path = os.path.join('output', image_path.split('/')[-1])
+cv2.imwrite(path, masked_image)
 
-cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+# cv2.namedWindow('img', cv2.WINDOW_NORMAL)
 cv2.namedWindow('masked', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('img', 500, 500)
+# cv2.resizeWindow('img', 500, 500)
 cv2.resizeWindow('masked', 500, 500)
 
-cv2.imshow('img', raw_image)
+# cv2.imshow('img', raw_image)
 cv2.imshow('masked', masked_image)
 cv2.waitKey(2000)
+
+
+
--- src/vision/kmeans_color_identifier/testing.py
+++ src/vision/kmeans_color_identifier/testing.py
@@ -5,9 +5,9 @@ dir = 'images/test2/'
 for image in listdir(dir):
     name = path.splitext(image)[0]
     extension = path.splitext(image)[1]
-    
     filename = dir + name + extension
     
     if (extension == '.jpg'):
         print(filename)
         system("python3 identify_colors.py " + filename)
+
