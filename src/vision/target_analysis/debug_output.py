05725300f8bd4a52a953e1375707291558006a69
Andy Peng <andypg20@gmail.com>
2019-05-30 17:07:27 -0700

    Fix output bugs

--- src/vision/target_analysis/debug_output.py
+++ src/vision/target_analysis/debug_output.py
@@ -1,3 +1,4 @@
+import os
 import cv2 as cv
 import matplotlib.pyplot as plt
 
@@ -9,18 +10,21 @@ OUTPUT_PATH = 'output'
 def showHistogram(img_id, hist, out_dir=OUTPUT_PATH):
     fig, ax = plt.subplots()
     ax.plot(hist)
-    fig.savefig(OUTPUT_PATH + '/histogram%03d.png'%img_id)
+    path = os.path.join(out_dir, 'histograms/histogram%03d.png'%img_id)
+    fig.savefig(path)
     plt.close(fig)
 
 # Save binary image to file
 def showThreshold(img_id, thresh, out_dir=OUTPUT_PATH):
-    cv.imwrite(OUTPUT_PATH + '/thresh%03d.jpg'%img_id, thresh)
+    path = os.path.join(out_dir, 'thresholds/thresh%03d.jpg'%img_id)
+    cv.imwrite(path, thresh)
 
 # Highlight and crop contour, then save and display
 def showContour(img_id, img, contour, out_dir=OUTPUT_PATH, display=False):
     x, y, w, h = cv.boundingRect(contour)
     imgContour = cv.drawContours(img.copy(), [contour], 0, (255, 255, 255), 5)[y:y+h, x:x+w]
-    cv.imwrite(OUTPUT_PATH + '/contour%03d.png'%img_id, imgContour)
+    path = os.path.join(out_dir, 'countours/contour%03d.png'%img_id)
+    cv.imwrite(path, imgContour)
     if display:
         cv.imshow('Contour', imgContour)
         cv.waitKey(2000)
--- src/vision/target_analysis/test_analysis.py
+++ src/vision/target_analysis/test_analysis.py
@@ -6,11 +6,13 @@ def test_all_in_dir(in_dir, out_dir, display=True):
         name = os.path.splitext(image)[0]
         extension = os.path.splitext(image)[1]
         if (extension == '.jpg'):
-            print(filename)
-            analyze_image(in_dir, name, out_dir=out_dir, display=display, debug=False)
+            analyze_one_image(in_dir, int(name), out_dir=out_dir, display=display, debug=True)
 
 
 if __name__=='__main__':
     os.makedirs('output/data', exist_ok=True)
+    os.makedirs('output/data/histograms', exist_ok=True)
+    os.makedirs('output/data/countours', exist_ok=True)
+    os.makedirs('output/data/thresholds', exist_ok=True)
     test_all_in_dir('data', 'output/data', False)
 
