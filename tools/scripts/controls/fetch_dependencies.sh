66f01c809350d8e65a6b2eff082022e30784af8b
Comran Morshed <comran.morshed@dji.com>
2019-07-05 10:33:48 -0700

    Show less verbose output on bazel fetch.

--- tools/scripts/controls/fetch_dependencies.sh
+++ tools/scripts/controls/fetch_dependencies.sh
@@ -7,7 +7,7 @@ BAZEL_FETCH_FLAGS="--noshow_loading_progress "
 if [ -n "$CONTINUOUS_INTEGRATION" ]
 then
   # Don't do parallel downloads in CI since it is unreliable on some machines.
-  BAZEL_FETCH_FLAGS="$BAZEL_FETCH_FLAGS --loading_phase_threads=1 "
+  BAZEL_FETCH_FLAGS="$BAZEL_FETCH_FLAGS --loading_phase_threads=1 --noshow_progress "
 fi
 
 while [ $ATTEMPTS -le 5 ]
