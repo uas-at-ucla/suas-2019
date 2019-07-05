54d9b6c5a44fd96bfd1a8fc652ba02a1f666aff4
Comran Morshed <comran.morshed@dji.com>
2019-07-05 15:07:33 -0700

    Store fetched dependencies on CI.

--- tools/scripts/controls/fetch_dependencies.sh
+++ tools/scripts/controls/fetch_dependencies.sh
@@ -3,12 +3,19 @@
 ATTEMPTS=0
 FAIL_WAIT=5
 
-BAZEL_FETCH_FLAGS=""
+BAZEL_FETCH_FLAGS="--repository_cache=$(pwd)/tools/cache/downloaded "
 if [ -n "$CONTINUOUS_INTEGRATION" ]
 then
   # Don't do parallel downloads in CI since it is unreliable on some machines.
   BAZEL_FETCH_FLAGS="$BAZEL_FETCH_FLAGS --loading_phase_threads=1 --noshow_progress "
 fi
+
+if [ -n "$JENKINS_URL" ] && [ -n "$JENKINS_NODE_COOKIE" ]
+then
+  echo "Caching to jenkins slave!"
+  BAZEL_FETCH_FLAGS="$BAZEL_FETCH_FLAGS --repository_cache=/tmp/bazel_downloaded "
+fi
+  
 
 while [ $ATTEMPTS -le 5 ]
 do
